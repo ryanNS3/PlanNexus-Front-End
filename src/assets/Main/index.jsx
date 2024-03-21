ef0b6464d7bd04a7ee7a9ded04a5e87f12cd6edc
samuelleitee <programmer.and.investor@gmail.com>
2024-03-21 16:00:30 -0300

    feat: create Main component

---
+++ src/assets/Main/index.jsx
@@ -0,0 +1,3 @@
+export function Main({ children }) {
+  return <main className="bg-rosa-50 h-4 w-full">{children}</main>;
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,9 +7,7 @@ export function Home() {
     <div className="grid grid-cols-12 gap-5 mx-8 max-w-[90rem]">
       <Sidebar className="col-span-2" />
       <Header className="col-span-10" />
-      <main>
         <Outlet />
-      </main>
     </div>
   );
 }
