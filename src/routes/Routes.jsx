99d3b409fa03906549fb001c5557daecca16b2d2
samuelleitee <programmer.and.investor@gmail.com>
2024-03-26 15:56:06 -0300

    refactor: changed structure of routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,11 +6,14 @@ import { Statistic } from "../components/Statistic";
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path=":page" element={<Statistic />}>
-          <Route path=":period" />
-        </Route>
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/estoque" element={<Home />}/>
+      <Route path="/doacao" element={<Home />}/>
+      <Route path="/financeiro" element={<Home />}/>
+      <Route path="/calendario" element={<Home />}/>
+      <Route path="/historico" element={<Home />}/>
+      <Route path="/gestao" element={<Home />}/>
+
     </Routes>
   );
 }
