6ea762701e11836a5568444643a60bb86ebc8ed1
samuelleitee <programmer.and.investor@gmail.com>
2024-03-26 15:38:34 -0300

    style: fixed responsive layout in sidebar - completed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -6,9 +6,9 @@ import { HistoricoIcon, HistoricoIconLight } from "../../assets/Historico";
 import { GestaoIcon, GestaoIconLight } from "../../assets/Gestao";
 import { HomeIcon, HomeIconLight } from "../../assets/Home";
 import Logo from "../../assets/logo.svg";
-import CompactLogo from "../../assets/compactLogo.svg"
+import CompactLogo from "../../assets/compactLogo.svg";
 
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export function Sidebar() {
@@ -21,43 +21,41 @@ export function Sidebar() {
 
       <nav>
         <ul className="flex flex-col gap-9">
-          <SidebarElement text="Home" icon="Home" link="/" />
-          <SidebarElement text="Estoque" icon="Estoque" link="estoque" />
-          <SidebarElement text="Doação" icon="Doação" link="doacao" />
+          <SidebarElement text="Home" icon="Home" href="/" />
+          <SidebarElement text="Estoque" icon="Estoque" href="/estoque" />
+          <SidebarElement text="Doação" icon="Doação" href="/doacao" />
           <SidebarElement
             text="Financeiro"
             icon="Financeiro"
-            link="financeiro"
+            href="/financeiro"
           />
           <SidebarElement
             text="Calendário"
             icon="Calendário"
-            link="calendario"
+            href="/calendario"
           />
-          <SidebarElement text="Histórico" icon="Histórico" link="historico" />
-          <SidebarElement text="Gestão" icon="Gestão" link="gestao" />
+          <SidebarElement text="Histórico" icon="Histórico" href="/historico" />
+          <SidebarElement text="Gestão" icon="Gestão" href="/gestao" />
         </ul>
       </nav>
     </aside>
   );
 }
 
-export function SidebarElement({ text, link, icon }) {
+import { useMatch, useResolvedPath } from "react-router-dom";
+
+export function SidebarElement({ text, href, icon }) {
   /* PROPS
    * text: STRING
-   * link: STRING
+   * href: STRING
    * icon: ENUM("estoque", "Estoque", "doação", "Doação", "financeiro", "Financeiro", "calendário", "Calendário", "histórico", "Histórico", "gestão", "Gestão")
    */
 
   const [isHovered, setIsHovered] = useState(false);
-
-  let { page } = useParams();
-
-  if (!page) page = "/";
+  const resolvedPath = useResolvedPath(href);
+  const isActive = useMatch({ path: resolvedPath.pathname });
 
   const navIcon = (icon) => {
-    const isActive = page === link;
-
     switch (icon) {
       case "Home":
       case "home":
@@ -136,10 +134,13 @@ export function SidebarElement({ text, link, icon }) {
 
   return (
     <li>
-      {/* <Link
-        to={link}
+      <Link
+        to={href}
         className={`flex items-center justify-start w-full gap-10 px-4 py-5 bg-red-500 rounded-lg font-poppins font-medium relative transition-all duration-150
 
+        // RESPONSIVE SIZES
+        
+
         // ESTILOS DO PSEUDO ELEMENTO AFTER
         
         hover:after:content-[''] hover:after:h-3/5 hover:after:w-1.5 hover:after:absolute hover:after:right-[-1rem] hover:after:bg-rosa-300 hover:after:rounded-lg
@@ -148,7 +149,7 @@ export function SidebarElement({ text, link, icon }) {
         hover:from-[#BD3FD1] hover:to-[#9332AE] hover:bg-gradient-to-b hover:text-[#fff]
         
         ${
-          page === link
+          isActive
             ? `
         shadow-[0_4px_16px_0px_rgba(245,208,255)]
 
@@ -158,39 +159,16 @@ export function SidebarElement({ text, link, icon }) {
         //GRADIENTE
         from-[#BD3FD1] to-[#9332AE] bg-gradient-to-b text-[#fff]`
             : `after:content-[''] after:h-0 after:w-0 after:absolute after:right-[-1rem] after:bg-rosa-300 after:rounded-lg`
-        }`}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      > */}
-      <Link
-        to={link}
-        className={`flex items-center justify-center w-fit gap-9 p-4 rounded-lg font-poppins font-medium relative transition-all duration-150 m-auto
-
-        // ESTILOS DO PSEUDO ELEMENTO AFTER
-        
-        hover:after:content-[''] hover:after:h-3/5 hover:after:w-1.5 hover:after:absolute hover:after:right-[-1rem] hover:after:bg-rosa-300 hover:after:rounded-lg
-
-        // HOVER
-        hover:from-[#BD3FD1] hover:to-[#9332AE] hover:bg-gradient-to-b hover:text-[#fff]
-
-        ${
-          page === link
-            ? `
-        shadow-[0_4px_16px_0px_rgba(245,208,255)]
-
-        // ESTILOS DO PSEUDO ELEMENTO AFTER
-        after:content-[''] after:h-3/5 after:w-1.5 after:absolute after:right-[-1rem] after:bg-rosa-300 after:rounded-lg
-
-        //GRADIENTE
-        from-[#BD3FD1] to-[#9332AE] bg-gradient-to-b text-[#fff]`
-            : `after:content-[''] after:h-0 after:w-0 after:absolute after:right-[-1rem] after:bg-rosa-300 after:rounded-lg`
-        }
         }`}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         {navIcon(icon)}
 
+        <span className="max-[1471px]:absolute max-[1471px]:left-20 max-[1471px]:px-4 max-[1471px]:py-2 max-[1471px]:bg-rosa-300 max-[1471px]:text-ct3 max-[1471px]:hidden rounded-md">
+          {text}
+        </span>
+        
         {isHovered ? (
           <span className="max-[1471px]:absolute max-[1471px]:left-20 max-[1471px]:px-4 max-[1471px]:py-2 max-[1471px]:bg-rosa-300 max-[1471px]:text-ct3 rounded-md text-branco">
             {text}
