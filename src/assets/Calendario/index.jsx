a2815b7a2ec3b4abf38b6fe7627c2b5b79650f0a
samuelleitee <programmer.and.investor@gmail.com>
2024-03-28 17:01:51 -0300

    style: change calendar icon sizes according to display

--- src/assets/Calendario/index.jsx
+++ src/assets/Calendario/index.jsx
@@ -1,6 +1,7 @@
 export function CalendarioIcon({ size }) {
   return (
     <svg
+      className="max-[1471px]:w-6 max-[1471px]:h-6 max-[1471px]:m-auto"
       xmlns="http://www.w3.org/2000/svg"
       height={`${size}`}
       viewBox="0 -960 960 960"
@@ -14,6 +15,7 @@ export function CalendarioIcon({ size }) {
 export function CalendarioIconLight({ size }) {
   return (
     <svg
+      className="max-[1471px]:w-6 max-[1471px]:h-6 max-[1471px]:m-auto"
       xmlns="http://www.w3.org/2000/svg"
       height={`${size}`}
       viewBox="0 -960 960 960"
