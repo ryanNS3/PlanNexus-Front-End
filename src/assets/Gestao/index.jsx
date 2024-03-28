8694080174a8f392017f747da368825ef5b692a7
samuelleitee <programmer.and.investor@gmail.com>
2024-03-28 17:02:53 -0300

    style: change management icon sizes according to display

--- src/assets/Gestao/index.jsx
+++ src/assets/Gestao/index.jsx
@@ -1,6 +1,7 @@
 export function GestaoIcon({ size }) {
   return (
     <svg
+      className="max-[1471px]:w-6 max-[1471px]:h-6 max-[1471px]:m-auto"
       xmlns="http://www.w3.org/2000/svg"
       height={`${size}`}
       viewBox="0 -960 960 960"
@@ -14,6 +15,7 @@ export function GestaoIcon({ size }) {
 export function GestaoIconLight({ size }) {
   return (
     <svg
+      className="max-[1471px]:w-6 max-[1471px]:h-6 max-[1471px]:m-auto"
       xmlns="http://www.w3.org/2000/svg"
       height={`${size}`}
       viewBox="0 -960 960 960"
