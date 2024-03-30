e63e00ffd052faf3ec419645b6be753250488557
LaizaBarbosa <laiza0700@gmail.com>
2024-03-30 17:15:28 -0300

    style: changed style to fit the page size

--- src/components/NavBarGestao/index.jsx
+++ src/components/NavBarGestao/index.jsx
@@ -3,31 +3,31 @@ import { useParams } from "react-router-dom";
 export function NavBarGestao() {
   const url = window.location;
   const turmasStyle =
-    url.pathname === "/home/turmas"
+    url.pathname === "/gestao/turmas"
       ? "bg-cinza-800 text-cinza-50 rounded-lg"
       : "bg-cinza-100";
 
   const funcionariosStyle =
-    url.pathname === "/home/funcionarios"
+    url.pathname === "/gestao/funcionarios"
       ? "bg-cinza-800 text-cinza-50 rounded-lg"
       : "bg-cinza-100";
   const armariosStyle =
-    url.pathname === "/home/armarios"
+    url.pathname === "/gestao/armarios"
       ? "bg-cinza-800 text-cinza-50 rounded-lg"
       : "bg-cinza-100";
 
-  const hoverTurmas = url.pathname === "/home/turmas" ? "" : "hover:border-b-2 hover:border-dashed"
-  const hoverFuncionarios = url.pathname === "/home/funcionarios" ? "" : "hover:border-b-2 hover:border-dashed"
-  const hoverArmarios = url.pathname === "/home/armarios" ? "" : "hover:border-b-2 hover:border-dashed"
+  const hoverTurmas = url.pathname === "/gestao/turmas" ? "" : "hover:border-b-2 hover:border-dashed"
+  const hoverFuncionarios = url.pathname === "/gestao/funcionarios" ? "" : "hover:border-b-2 hover:border-dashed"
+  const hoverArmarios = url.pathname === "/gestao/armarios" ? "" : "hover:border-b-2 hover:border-dashed"
 
   return (
     <>
-      <nav className="flex bg-cinza-100 rounded-lg text-cinza-800 text-fun2 w-full max-w-[71.438rem] min-w-[23.813rem]">
+      <nav className="flex bg-cinza-100 rounded-lg text-cinza-800 text-fun2 w-full mt-12 max-w-[74.188] min-w-[23.813rem]">
         <ul className="flex gap-2">
           <li className={`py-4 px-5 ${turmasStyle}`}>
             <a
               className={`${hoverTurmas} block`}
-              href="/home/turmas"
+              href="/gestao/turmas"
             >
               Turmas
             </a>
@@ -35,7 +35,7 @@ export function NavBarGestao() {
           <li className={`py-4 px-5 ${funcionariosStyle}`}>
             <a
               className={`${hoverFuncionarios} block`}
-              href="/home/funcionarios"
+              href="/gestao/funcionarios"
             >
               Funcionários
             </a>
@@ -43,7 +43,7 @@ export function NavBarGestao() {
           <li className={`py-4 px-5 ${armariosStyle}`}>
             <a
               className={`${hoverArmarios} block`}
-              href="/home/armarios"
+              href="/gestao/armarios"
             >
               Armários
             </a>
