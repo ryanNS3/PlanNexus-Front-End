0f15009391e75df28cae3c3112375adffdc16cb6
samuelleitee <programmer.and.investor@gmail.com>
2024-04-02 16:29:20 -0300

    style: add responsiviness in Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,15 +10,21 @@ import { Statistic } from "../../components/Statistic";
 
 export function Home() {
   return (
-    <div className="grid grid-cols-12 gap-5 mx-8 max-w-[90rem]">
+    <div className="grid grid-cols-12 gap-5 mx-8 max-w-[90rem]" style={{margin: 'auto'}}>
       <Sidebar />
       <Main>
         <Header />
         <Statistic />
-        <Outlet />
-        <SalesChart />
-        <Grafico />
-        <Agenda />
+
+        <div className="grid grid-cols-[1fr_auto] mt-18 gap-6">
+          <h5 className="col-span-full text-h5">Geral</h5>
+          <div className="gap-6">
+            <SalesChart />
+            <Grafico />
+          </div>
+          <Agenda />
+        </div>
+
       </Main>
     </div>
   );
