ad470148603a0ab25cc0b184320585dbffa3c63c
samuelleitee <programmer.and.investor@gmail.com>
2024-04-02 14:46:46 -0300

    feat: add structure of Agenda component

--- src/components/Agenda/index.jsx
+++ src/components/Agenda/index.jsx
@@ -1,5 +1,19 @@
 export function Agenda() {
     return (
-        <></>
+        <section>
+            <h2 className="text-sub2">Agenda</h2>
+
+            <nav>
+                <ul className="flex gap-6">
+                    <li className="cursor-pointer w-8 h-8 rounded-full flex items-center justify-center bg-rosa-300 text-fun2 text-branco">S</li>
+                    <li className="cursor-pointer w-8 h-8 rounded-full flex items-center justify-center bg-cinza-100 text-fun2 text-cinza-500 hover:bg-rosa-300 hover:text-branco">T</li>
+                    <li className="cursor-pointer w-8 h-8 rounded-full flex items-center justify-center bg-cinza-100 text-fun2 text-cinza-500 hover:bg-rosa-300 hover:text-branco">Q</li>
+                    <li className="cursor-pointer w-8 h-8 rounded-full flex items-center justify-center bg-cinza-100 text-fun2 text-cinza-500 hover:bg-rosa-300 hover:text-branco">Q</li>
+                    <li className="cursor-pointer w-8 h-8 rounded-full flex items-center justify-center bg-cinza-100 text-fun2 text-cinza-500 hover:bg-rosa-300 hover:text-branco">S</li>
+                    <li className="cursor-pointer w-8 h-8 rounded-full flex items-center justify-center bg-cinza-100 text-fun2 text-cinza-500 hover:bg-rosa-300 hover:text-branco">S</li>
+                    <li className="cursor-pointer w-8 h-8 rounded-full flex items-center justify-center bg-cinza-100 text-fun2 text-cinza-500 hover:bg-rosa-300 hover:text-branco">D</li>
+                </ul>
+            </nav>
+        </section>
     )
 }
