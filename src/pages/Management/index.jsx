222381835d846dabef5a4649299928b8196ea347
samuelleitee <programmer.and.investor@gmail.com>
2024-04-02 17:21:21 -0300

    style: identation in the Management component

--- src/pages/Management/index.jsx
+++ src/pages/Management/index.jsx
@@ -6,7 +6,7 @@ import { Header } from "../../components/Header";
 
 export function Management(){
     return(
-        <div className="grid grid-cols-12 gap-5 mx-8 max-w-[90rem]" style={{margin: 'auto'}}>
+            <div className="grid grid-cols-12 gap-5 mx-8 max-w-[90rem]" style={{margin: 'auto'}}>
                 <Sidebar/>
             <Main>
                 <Header/>
