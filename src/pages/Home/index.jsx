ac557759f0a675e41f63835b2ff3841b0667f516
samuelleitee <programmer.and.investor@gmail.com>
2024-04-02 14:33:12 -0300

    feat: create Agenda component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,24 @@
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
-import { Grafico } from "../../components/GraphicBar"
+import { Grafico } from "../../components/GraphicBar";
 import { Main } from "../../components/Main";
 import { SalesChart } from "../../components/SalesChart";
 import { Outlet } from "react-router-dom";
 import { NavBarGestao } from "../../components/NavBarGestao";
+import { Agenda } from "../../components/Agenda";
 import { Statistic } from "../../components/Statistic";
 
 export function Home() {
   return (
     <div className="grid grid-cols-12 gap-5 mx-8 max-w-[90rem]">
-      <Sidebar/>
+      <Sidebar />
       <Main>
-        <Header/>
-        <Statistic/>
-        <Outlet/>
-        <SalesChart/>
-        <Grafico/>
+        <Header />
+        <Statistic />
+        <Outlet />
+        <SalesChart />
+        <Grafico />
+        <Agenda />
       </Main>
     </div>
   );
