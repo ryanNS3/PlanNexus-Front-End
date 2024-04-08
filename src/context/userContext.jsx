680b38e8003d292ac96f4cac6a8fec5a006fa763
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-07 23:01:00 -0300

    Update userContext.jsx

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -11,7 +11,7 @@ export const UserProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [error, setError] = useState(null);
 
-  const userLoginRequest = async (email, password) => {
+  const userLoginRequest = async ({email, password}) => {
     setLoading(true);
     try {
       const response = await requisicao(
