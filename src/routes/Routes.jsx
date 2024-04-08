e4e6d2ddd4ad1b25f793d2c9a8f14d8396be6d93
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-07 22:11:31 -0300

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,19 +2,30 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Statistic } from "../components/Statistic";
 import { Management } from "../pages/Management";
+import { Login } from "../pages/login";
+import { UserProvider } from "../context/userContext";
+import { ProtectRouter } from "../components/protectRouter/protectRouter";
 
 export function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/estoque" element={<Home />}/>
-      <Route path="/doacao" element={<Home />}/>
-      <Route path="/financeiro" element={<Home />}/>
-      <Route path="/historico" element={<Home />}/>
-      <Route path="/gestao" element={<Management />}/>
-
-      <Route path="/gestao/turmas" element={<Management/>}/>
-      <Route path="/gestao/funcionarios" element={<Management/>}/>
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/estoque" element={<Home />}/>
+        <Route path="/doacao" element={<Home />}/>
+        <Route path="/financeiro" element={<Home />}/>
+        <Route path="/calendario" element={
+        <ProtectRouter>
+          <Home />
+        </ProtectRouter>
+        }/>
+        <Route path="/historico" element={<Home />}/>
+        <Route path="/gestao" element={<Management />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/gestao/turmas" element={<Management/>}/>
+        <Route path="/gestao/funcionarios" element={<Management/>}/>
+      </Routes>
+    </UserProvider>
+    
   );
 }
