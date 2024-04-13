0223025bc6a46ae05bbbd8ebf0d7d6d1fbe971c3
LaizaBarbosa <laiza0700@gmail.com>
2024-04-13 18:51:05 -0300

    style: added variable from new style library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import withMT from "@material-tailwind/react/utils/withMT"
 
-  
+export default withMT({
   
   content: [
     "./index.html",
@@ -190,4 +190,4 @@ export default {
     },
   },
   plugins: [],
-}
+})
