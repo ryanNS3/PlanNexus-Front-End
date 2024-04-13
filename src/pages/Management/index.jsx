7aab822b0fcfeb042f82166c55e98fd95e3f0a6d
LaizaBarbosa <laiza0700@gmail.com>
2024-04-13 18:46:14 -0300

    fix: removed unused imports

--- src/pages/Management/index.jsx
+++ src/pages/Management/index.jsx
@@ -1,27 +1,20 @@
 import { TemplateView } from "../../components/ViewTemplate";
 import { Sidebar } from "../../components/Sidebar";
-import { NavBarGestao } from "../../components/NavBarGestao";
+import { ManagementTab } from "../../components/ManagementTab";
 import { Main } from "../../components/Main";
 import { Header } from "../../components/Header";
 
 import { AllLocker } from "../../components/AllLocker";
 
-import BasicModal from "../../components/Modal";
-import { EmployeeForm } from "../../components/Form/employee";
-
 
 export function Management(){
-    const url = window.location;
 
     return(
             <div className="grid grid-cols-12 gap-5  mx-8 max-w-[90rem]" style={{margin: 'auto'}}>
                 <Sidebar/>
                 <Main>
                     <Header/>
-                    <NavBarGestao/>
-                    <TemplateView>
-                        <EmployeeForm/>
-                    </TemplateView>
+                    <ManagementTab/>
                 </Main>
         </div>
 
