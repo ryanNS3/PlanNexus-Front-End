09da44a8314828053625def50facff5e38f84d26
LaizaBarbosa <laiza0700@gmail.com>
2024-04-13 18:49:07 -0300

    fix: removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Sidebar } from "./components/Sidebar";
-import { Header } from "./components/Header";
-import { CardAccept } from "./components/Cards/cardAccept";
-import { CardEvents } from "./components/Cards/cardEvents";
-import { Home } from "../src/pages/Home";
-import { Login } from "../src/pages/login";
-import { LineTable } from "./components/LineTable";
-import { NavBarGestao } from "./components/NavBarGestao";
-import { Statistic } from "./components/Statistic";
 import { useEffect } from "react";
 import { AcessibilityReporter } from "./utils/acessibility/Acessibility";
 import { Router } from "./routes/Routes";
