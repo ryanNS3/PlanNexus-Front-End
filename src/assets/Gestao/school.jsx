9c39ab0d05b4e7680fab73952af403eb652b336d
LaizaBarbosa <laiza0700@gmail.com>
2024-04-13 18:47:31 -0300

    feat: added icons for tab component

---
+++ src/assets/Gestao/school.jsx
@@ -0,0 +1,13 @@
+export function SchoolIcon(){
+    return(
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<mask id="mask0_2476_2213" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="16" height="16">
+<rect width="16" height="16" fill="#D9D9D9"/>
+</mask>
+<g mask="url(#mask0_2476_2213)">
+<path d="M9.22224 16L3.7778 13.0444V8.37778L0.666687 6.66667L9.22224 2L17.7778 6.66667V12.8889H16.2222V7.52222L14.6667 8.37778V13.0444L9.22224 16ZM9.22224 9.54444L14.55 6.66667L9.22224 3.78889L3.89446 6.66667L9.22224 9.54444ZM9.22224 14.2306L13.1111 12.1306V9.19444L9.22224 11.3333L5.33335 9.19444V12.1306L9.22224 14.2306Z" fill="#F9F9F9"/>
+</g>
+</svg>
+
+    )
+}
