6a861f5e1341a63588c1fb6aefb8343c46c068ed
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-15 17:26:26 -0300

    Update protectRouter.jsx

--- src/components/protectRouter/protectRouter.jsx
+++ src/components/protectRouter/protectRouter.jsx
@@ -4,6 +4,5 @@ import { Navigate } from 'react-router-dom'
 
 export const ProtectRouter = ({ children }) => {
   const { token } = useContext(UserGlobal); 
-  console.log(token)
   return token ? children : <Navigate to="/login" />; 
 }
