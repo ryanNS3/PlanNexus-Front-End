9abaf0bf47f73dcca910927b7fb2b6ade17bee45
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-15 00:53:23 -0300

    Add files via upload

--- src/components/protectRouter/protectRouter.jsx
+++ src/components/protectRouter/protectRouter.jsx
@@ -3,6 +3,14 @@ import { UserGlobal } from '../../context/userContext';
 import { Navigate } from 'react-router-dom';
 
 export const ProtectRouter = ({ children }) => {
-  const { token } = useContext(UserGlobal); 
-  return token ? children : <Navigate to="/login" />;
+  const { token, user } = useContext(UserGlobal); 
+
+  const response = requisicao(
+        "http://172.16.3.83:3333/funcionario/token",
+         null,
+        "POST",
+        { NIF: user.data.NIF, token: token}
+      );
+  return response && response.status === 200 ? children : <Navigate to="/login" />;
+
 };
