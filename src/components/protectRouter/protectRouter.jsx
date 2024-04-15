db7103726fdb2df0e44cb157bfdca31310ae0bf5
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-15 14:33:43 -0300

    Update protectRouter.jsx

--- src/components/protectRouter/protectRouter.jsx
+++ src/components/protectRouter/protectRouter.jsx
@@ -1,16 +1,9 @@
-import { useContext } from 'react';
-import { UserGlobal } from '../../context/userContext';
-import { Navigate } from 'react-router-dom';
+import { useContext } from 'react'
+import { UserGlobal } from '../../context/userContext'
+import { Navigate } from 'react-router-dom'
 
 export const ProtectRouter = ({ children }) => {
-  const { token, user } = useContext(UserGlobal); 
-
-  const response = requisicao(
-        "http://172.16.3.83:3333/funcionario/token",
-         null,
-        "POST",
-        { NIF: user.data.NIF, token: token}
-      );
-  return response && response.status === 200 ? children : <Navigate to="/login" />;
-
-};
+  const { token } = useContext(UserGlobal); 
+  console.log(token)
+  return token ? children : <Navigate to="/login" />; 
+}
