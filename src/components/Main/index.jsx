2b7a6f655290a9484ef8d5414a164015ae1be420
PauloBezz <paulo.bezerra2057@gmail.com>
2024-04-15 15:32:49 -0300

    fix: add route locker, sidebar fixed and navigation in managent page

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,3 +1,3 @@
 export function Main({ children }) {
-  return <main className="col-start-2 min-[1471px]:col-start-3 col-end-[13] max-h-screen overflow-y-auto">{children}</main>;
+  return <main className="col-start-2 min-[1471px]:col-start-3 col-end-[13] max-h-screen">{children}</main>;
 }
--- src/pages/Management/index.jsx
+++ src/pages/Management/index.jsx
@@ -10,19 +10,19 @@ import BasicModal from "../../components/Modal";
 import { EmployeeForm } from "../../components/Form/employee";
 
 
-export function Management(){
+export function Management() {
     const url = window.location;
 
-    return(
-            <div className="grid grid-cols-12 gap-5  mx-8 max-w-[90rem]" style={{margin: 'auto'}}>
-                <Sidebar/>
-                <Main>
-                    <Header/>
-                    <NavBarGestao/>
-                    <TemplateView>
-                        <EmployeeForm/>
-                    </TemplateView>
-                </Main>
+    return (
+        <div className="grid grid-cols-12 gap-5  mx-8 max-w-[90rem]" style={{ margin: 'auto' }}>
+            <Sidebar />
+            <Main>
+                <Header />
+                <NavBarGestao />
+                {url.pathname === "/gestao/armarios" ? <AllLocker /> : <TemplateView>
+                    <EmployeeForm />
+                </TemplateView>}
+            </Main>
         </div>
 
     )
