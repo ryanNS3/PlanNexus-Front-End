623e6b065551c724c1e1bc671a9e607ba5abb666
samuelleitee <programmer.and.investor@gmail.com>
2024-04-17 17:21:56 -0300

    feat: add inputs to 1st step of AddStudent component

--- src/components/AddStudent/index.jsx
+++ src/components/AddStudent/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { PinkButton } from "../Buttons/pinkButton";
+import { InputText } from "../Inputs/input-text/inputTextComp";
 
 export function AddStudent() {
   const steps = [
@@ -20,13 +21,18 @@ export function AddStudent() {
         <nav>
           <ul className="flex justify-between">
             {steps?.map((step, index) => (
-              <li key={index} className="flex flex-col items-center cursor-pointer" onClick={() => setStep(index + 1)}>
+              <li key={index} className="flex flex-col items-center cursor-pointer gap-2" onClick={() => setStep(index + 1)}>
                 <div className={`flex rounded-full w-11 h-11 text-fun2 items-center justify-center ${currentStep == index + 1 ? "bg-rosa-300 text-rosa-50" : "bg-[#D9D9D9] text-cinza-700"}`}>{index + 1}</div>
                 <span className={`block ${currentStep == index + 1 ? "text-preto" : "text-cinza-500"} text-fun2 max-w-28 text-center`}>{step}</span>
               </li>
             ))}
           </ul>
         </nav>
+        <InputText id="name" type="text" name="Nome Completo" placeholder="ex:000.000.000-00"/> 
+        <InputText id="cpf" type="text" name="CPF" placeholder="ex:marlene"/> 
+        <InputText id="email" type="email" name="Email" placeholder="ex:marlene@gmail.com"/> 
+        <InputText id="phone" type="text" name="Telefone" placeholder="ex: 55 11 9999999"/> 
+        
         <PinkButton text="Voltar" />
         <PinkButton text="Continuar" />
       </div>
