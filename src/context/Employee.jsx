739504513e84bac0c26ddbe9e2bc3e45b70ff8fd
ryanNS3 <ryansantosn333@gmail.com>
2024-04-18 17:25:09 -0300

    feat/delete employee

--- src/context/Employee.jsx
+++ src/context/Employee.jsx
@@ -1,44 +1,56 @@
-import React from "react"
-import useAxios from "../hooks/useAxios"
+import React, { useContext } from "react";
+import useAxios from "../hooks/useAxios";
+import { UserGlobal } from "./userContext";
 
-const EmployeeContext = React.createContext()
+const EmployeeContext = React.createContext();
 
-export function EmployeeProvider({children}) {
-    const {requisicao} = useAxios();
-    const [EmployeeData, setEmployeeData] = React.useState(null)
+export function EmployeeProvider({ children }) {
+  const { requisicao } = useAxios();
+  const {token, user} = useContext(UserGlobal)
+  const [EmployeeData, setEmployeeData] = React.useState(null);
 
-    const GetEmployee = React.useCallback(async()=>{
-        try{
-            const res = await requisicao(
-              `${BASE_URL}/funcionario/todos`,
-              null,
-              `GET`,
-              {
-                token: token,
-                nif: user,
-              }
-            );
-            setEmployeeData(dados);
-          }
-          
-        catch(err){
-            console.log(err)
+  
+
+  const GetEmployee = React.useCallback(async () => {
+    try {
+      const res = await requisicao(
+        `${BASE_URL}/funcionario/todos`,
+        null,
+        `GET`,
+        {
+          token: token,
+          nif: user,
         }
+      );
+      setEmployeeData(dados);
+    } catch (err) {
+        console.log(err);
+    }
+},[]);
 
-    })
-
-
-    async function PutEmployee(){
-        
-
+const DeleteEmployee = React.useCallback(async() =>{
+    try{
+        const res = await requisicao(
+          `${BASE_URL}/funcionario/todos`,
+          null,
+          `DELETE`,
+          {
+            token: token,
+            nif: user,
+          }
+        );
+        setEmployeeData(dados);  
     }
 
-    
-        return <EmployeeContext.Provider value={{GetEmployee}}>{children}</EmployeeContext.Provider>
-
+    catch(err){
+        console.log(err)
     }
-    
 
+  },[]);
 
-
-
+  return (
+    <EmployeeContext.Provider value={{ GetEmployee, DeleteEmployee }}>
+      {children}
+    </EmployeeContext.Provider>
+  );
+}
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,96 +8,99 @@ import { ModalProvider } from "../context/modalContext";
 import { ToastifyProvider } from "../context/toastifyContext";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { EmployeeProvider } from "../context/Employee";
 
 export function Router() {
   return (
     <UserProvider>
-      <ToastifyProvider>
-        <ModalProvider>
-          <ToastContainer />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/"
-              element={
-                <ProtectRouter>
-                  <Home />
-                </ProtectRouter>
-              }
-            />
-            <Route
-              path="/estoque"
-              element={
-                <ProtectRouter>
-                  <Home />
-                </ProtectRouter>
-              }
-            />
+      <EmployeeProvider>
+        <ToastifyProvider>
+          <ModalProvider>
+            <ToastContainer />
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/"
+                element={
+                  <ProtectRouter>
+                    <Home />
+                  </ProtectRouter>
+                }
+                />
+              <Route
+                path="/estoque"
+                element={
+                  <ProtectRouter>
+                    <Home />
+                  </ProtectRouter>
+                }
+                />
 
-            <Route
-              path="/doacao"
-              element={
-                <ProtectRouter>
-                  <Home />
-                </ProtectRouter>
-              }
-            />
+              <Route
+                path="/doacao"
+                element={
+                  <ProtectRouter>
+                    <Home />
+                  </ProtectRouter>
+                }
+              />
 
-            <Route
-              path="/financeiro"
-              element={
-                <ProtectRouter>
-                  <Home />
-                </ProtectRouter>
-              }
-            />
+              <Route
+                path="/financeiro"
+                element={
+                  <ProtectRouter>
+                    <Home />
+                  </ProtectRouter>
+                }
+                />
 
-            <Route
-              path="/calendario"
-              element={
-                <ProtectRouter>
-                  <Home />
-                </ProtectRouter>
-              }
-            />
+              <Route
+                path="/calendario"
+                element={
+                  <ProtectRouter>
+                    <Home />
+                  </ProtectRouter>
+                }
+                />
 
-            <Route
-              path="/historico"
-              element={
-                <ProtectRouter>
-                  <Home />
-                </ProtectRouter>
-              }
-            />
+              <Route
+                path="/historico"
+                element={
+                  <ProtectRouter>
+                    <Home />
+                  </ProtectRouter>
+                }
+                />
 
-            <Route
-              path="/gestao"
-              element={
-                <Management />
-                // <ProtectRouter>
-                // </ProtectRouter>
-              }
-            />
+              <Route
+                path="/gestao"
+                element={
+                  <Management />
+                  // <ProtectRouter>
+                  // </ProtectRouter>
+                }
+              />
 
-            <Route
-              path="/gestao/turmas"
-              element={
-                <Management />
-                // <ProtectRouter>
-                // </ProtectRouter>
-              }
-            />
-            <Route
-              path="/gestao/funcionarios"
-              element={
-                <Management />
-                // <ProtectRouter>
-                // </ProtectRouter>
-              }
-            />
-          </Routes>
-        </ModalProvider>
-      </ToastifyProvider>
+              <Route
+                path="/gestao/turmas"
+                element={
+                  <Management />
+                  // <ProtectRouter>
+                  // </ProtectRouter>
+                }
+                />
+              <Route
+                path="/gestao/funcionarios"
+                element={
+                  <Management />
+                  // <ProtectRouter>
+                  // </ProtectRouter>
+                }
+                />
+            </Routes>
+          </ModalProvider>
+        </ToastifyProvider>
+      </EmployeeProvider>
     </UserProvider>
   );
 };
