ea274b1c4bb2001ba27a9209abe9ac332ae3b0e9
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-18 17:26:18 -0300

    feat: lineTable fetchData updated

--- src/components/LineTable/index.jsx
+++ src/components/LineTable/index.jsx
@@ -1,41 +1,48 @@
 import avatar from "../../assets/avatar.jpg";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useMemo } from "react";
 import axios from "axios";
 import useAxios from "../../hooks/useAxios";
 import { UserGlobal } from "../../context/userContext";
 
 export function LineTable() {
+  const { user, token } = useContext(UserGlobal);
   const [Employees, setEmployees] = useState([])
   const BASE_URL = import.meta.env.VITE_API_URL
   const {requisicao, dados} = useAxios()
-  const token = window.localStorage.getItem('token');
-  const user =  window.localStorage.getItem('user');
-
-  console.log(user)
-
-  async function requisicao1() {
-        const res = await requisicao(`${BASE_URL}/funcionario/todos`, null , `GET`,
-        
-          {
-            token: token,
-            nif: user
-          }
-        
-      )
-      setEmployees(dados)
-
-  }
 
   useEffect(() => {
-      requisicao1();
-    }, []);
+    async function fetchData() {
+      try {
+        const res = await requisicao(
+          `${BASE_URL}/funcionario/todos`,
+          null,
+          `GET`,
+          {
+            token: token,
+            nif: user,
+          }
+        );
+     
+        if (res && res.res.status === 200) {
+          setEmployees(res.dados);
+        }
+      } catch (error) {
+        console.log("Requisição falhou:", error);
+      }
+    }
+    fetchData(); // Chama a função de busca de dados na montagem do componente
+  }, []);
+  
+
+ 
+  
   
   if (!dados){
     return (
       <>
         {Employees.map((employee, key) => (
           
-            <div key={employee.NIF} className="rounded-lg w-full py-[0.875rem] pr-9 pl-4 border-2 border-cinza-100 bg-white flex justify-between">
+          <div key={employee.NIF} className="rounded-lg w-full py-[0.875rem] pr-9 pl-4 border-2 border-cinza-100 bg-white flex justify-between">
           <div className="flex items-center justify-center gap-[0.781rem]">
             <img src={avatar} className="rounded-full" height={36} width={36} />
             <p className="text-xs tracking-[0.01em] ">{NIF}</p>
