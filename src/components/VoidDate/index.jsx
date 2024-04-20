00e505368fdd864cd7122a1a75f7364b347fc08f
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-04-20 18:04:59 -0300

    feat: responsive

--- src/components/VoidDate/index.jsx
+++ src/components/VoidDate/index.jsx
@@ -7,7 +7,7 @@ export function VoidDate(){
 
     return (
         <>
-      <div className="col-span-2 col-start-3 w-52 h-24 p-4 mt-8 shadow-lg flex-col flex justify-between">
+      <div className=" w-52 h-24 p-4 shadow-lg flex-col flex justify-between">
         <div className="flex justify-between items-center">
           <p className="text-fun2">Esvaziamento</p>
         <CalenBlack/>
