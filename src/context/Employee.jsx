1565b2ed04cc1f770fe22071f123c4f0b41c2409
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-23 17:28:16 -0300

    Update Employee.jsx

--- src/context/Employee.jsx
+++ src/context/Employee.jsx
@@ -2,14 +2,13 @@ import React, { useContext } from "react";
 import useAxios from "../hooks/useAxios";
 import { UserGlobal } from "./userContext";
 
-const EmployeeContext = React.createContext();
+export const EmployeeContext = React.createContext();
 
 export function EmployeeProvider({ children }) {
   const { requisicao } = useAxios();
-  const {token, user} = useContext(UserGlobal)
+  const { token, user } = useContext(UserGlobal);
   const [EmployeeData, setEmployeeData] = React.useState(null);
-
-  
+  const BASE_URL = import.meta.env.VITE_API_URL;
 
   const GetEmployee = React.useCallback(async () => {
     try {
@@ -18,38 +17,80 @@ export function EmployeeProvider({ children }) {
         null,
         `GET`,
         {
-          token: token,
+          authorization: `bearer ${token}`,
           nif: user,
         }
       );
-      setEmployeeData(dados);
-    } catch (err) {
-        console.log(err);
+      console.log(res)
+      if (res && res.res.status === 200) {
+        setEmployeeData(res.json.response);
+      }
+    } catch (error) {
+      console.log("Requisição falhou:", error);
     }
-},[]);
+  }, []);
 
-const DeleteEmployee = React.useCallback(async() =>{
-    try{
-        const res = await requisicao(
-          `${BASE_URL}/funcionario/todos`,
-          null,
-          `DELETE`,
-          {
-            token: token,
-            nif: user,
-          }
-        );
-        setEmployeeData(dados);  
+  const AddEmployee = React.useCallback(async () => {
+    try {
+      const res = await requisicao(
+        `${BASE_URL}/funcionario/todos`,
+        null,
+        `POST`,
+        {
+          authorization: `bearer ${token}`,
+          nif: user,
+        }
+      );
+      if (res && res.res.status === 200) {
+        setEmployeeData(res.json.response);
+      }
+    } catch (error) {
+      console.log("Requisição falhou:", error);
     }
+  }, []);
 
-    catch(err){
-        console.log(err)
+  const EditEmployee = React.useCallback(async () => {
+    try {
+      const res = await requisicao(
+        `${BASE_URL}/funcionario/todos`,
+        null,
+        `PUT`,
+        {
+          authorization: `bearer ${token}`,
+          nif: user,
+        }
+      );
+      if (res && res.res.status === 200) {
+        setEmployeeData(res.json.response);
+      }
+    } catch (error) {
+      console.log("Requisição falhou:", error);
     }
+  }, []);
 
-  },[]);
+  const DeleteEmployee = React.useCallback(async () => {
+    try {
+      const res = await requisicao(
+        `${BASE_URL}/funcionario/todos`,
+        null,
+        `DELETE`,
+        {
+          authorization: `bearer ${token}`,
+          nif: user,
+        }
+      );
+      if (res && res.res.status === 200) {
+        setEmployeeData(res.json.response);
+      }
+    } catch (error) {
+      console.log("Requisição falhou:", error);
+    }
+  }, []);
 
   return (
-    <EmployeeContext.Provider value={{ GetEmployee, DeleteEmployee }}>
+    <EmployeeContext.Provider
+      value={{ GetEmployee, DeleteEmployee, AddEmployee, EditEmployee, EmployeeData }}
+    >
       {children}
     </EmployeeContext.Provider>
   );
