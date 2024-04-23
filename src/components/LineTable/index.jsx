1ea0784795929c2ec2b6bee6c39d4b452290a669
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-23 17:28:58 -0300

    Update index.jsx

--- src/components/LineTable/index.jsx
+++ src/components/LineTable/index.jsx
@@ -1,68 +1,46 @@
 import avatar from "../../assets/avatar.jpg";
 import { useState, useEffect, useContext, useMemo } from "react";
-import axios from "axios";
-import useAxios from "../../hooks/useAxios";
 import { UserGlobal } from "../../context/userContext";
+import { EmployeeContext } from "../../context/Employee";
 
 export function LineTable() {
   const { user, token } = useContext(UserGlobal);
-  const [Employees, setEmployees] = useState([])
-  const BASE_URL = import.meta.env.VITE_API_URL
-  const {requisicao, dados} = useAxios()
+  const { GetEmployee, EmployeeData } = useContext(EmployeeContext);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await requisicao(
-          `${BASE_URL}/funcionario/todos`,
-          null,
-          `GET`,
-          {
-            token: token,
-            nif: user,
-          }
-        );
-     
-        if (res && res.res.status === 200) {
-          setEmployees(res.dados);
-        }
-      } catch (error) {
-        console.log("Requisição falhou:", error);
-      }
-    }
-    fetchData(); // Chama a função de busca de dados na montagem do componente
+    GetEmployee();
   }, []);
-  
 
- 
-  
-  
-  if (!dados){
+  if (EmployeeData && EmployeeData.length > 0) {
     return (
       <>
-        {Employees.map((employee, key) => (
-          
-          <div key={employee.NIF} className="rounded-lg w-full py-[0.875rem] pr-9 pl-4 border-2 border-cinza-100 bg-white flex justify-between">
-          <div className="flex items-center justify-center gap-[0.781rem]">
-            <img src={avatar} className="rounded-full" height={36} width={36} />
-            <p className="text-xs tracking-[0.01em] ">{NIF}</p>
-          </div>
-          <div className="flex items-center justify-center gap-20">
-            <div className="bg-[#64B140] rounded px-4 py-2">
-              <p className="text-[#fff]">Sim</p>
+        {EmployeeData.map((employee, key) => (
+          <div
+            key={key}
+            className="rounded-lg w-full py-[0.875rem] pr-9 pl-4 border-2 border-cinza-100 bg-white flex justify-between"
+          >
+            <div className="flex items-center justify-center gap-[0.781rem]">
+              <img
+                src={avatar}
+                className="rounded-full"
+                height={36}
+                width={36}
+              />
+              <p className="text-xs tracking-[0.01em] ">{employee.nome}</p>
             </div>
-            <div className="flex gap-1 cursor-pointer p-2 hover:bg-cinza-100 rounded">
-              <div className="rounded-full bg-cinza-400 height w-2 h-2"></div>
-              <div className="rounded-full bg-cinza-400 height w-2 h-2"></div>
-              <div className="rounded-full bg-cinza-400 height w-2 h-2"></div>
+            <div className="flex items-center justify-center gap-20">
+              <div className="bg-[#64B140] rounded px-4 py-2">
+                <p className="text-[#fff]">Sim</p>
+              </div>
+              <div className="flex gap-1 cursor-pointer p-2 hover:bg-cinza-100 rounded">
+                <div className="rounded-full bg-cinza-400 height w-2 h-2"></div>
+                <div className="rounded-full bg-cinza-400 height w-2 h-2"></div>
+                <div className="rounded-full bg-cinza-400 height w-2 h-2"></div>
+              </div>
             </div>
           </div>
-        </div>
-          
         ))}
       </>
     );
-  }
-
-  else return null
+  } else return null;
 }
