ab09665d9ca90f67861274f116efd2cb2f9c4c57
ryanNS3 <ryansantosn333@gmail.com>
2024-04-27 20:39:20 -0300

    fix: melhoria na acessibilidade dos cards

--- src/components/Cards/Card/index.jsx
+++ src/components/Cards/Card/index.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 export function CardSmall({children}){
   return (
-    <div className='px-4 py-6 flex flex-col gap-12 border border-cinza-100 rounded-lg shadow-[0_4px_8px_0px_rgba(227,227,227)]'>
+    <article className='px-4 py-6 flex flex-col gap-12 border border-cinza-100 rounded-lg shadow-[0_4px_8px_0px_rgba(227,227,227)]'>
         {children}
-    </div>
+    </article>
   )
 }
 
 export function CardMedium({children}){
     return(
-        <div className=' max-w-80 px-4 py-6 flex flex-col gap-12 border border-cinza-100 rounded-lg shadow-[0_4px_8px_0px_rgba(227,227,227)]'>
+        <article className='  max-w-86 px-4 py-6 flex flex-col gap-12 border border-cinza-100 rounded-lg shadow-[0_4px_8px_0px_rgba(227,227,227)]'>
             {children}
-        </div>
+        </article>
     )
 }
 
 
 export function CardLarge({children}){
     return(
-        <div>
+        <article>
             {children}
-        </div>
+        </article>
     )
 }
