3f238496edd1592af88c4735d020c8c6ab5a377d
ryanNS3 <ryansantosn333@gmail.com>
2024-04-29 00:57:17 -0300

    feat: Criação do checkBox de tamanhos e cores

---
+++ src/components/Inputs/CheckBox/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function SquareCheckBox({name, value , setvalue, check, onChange, ...props}){
+  return (
+    <label className=' block relative '> 
+        <div className='py-4 px-5 bg-cinza-100 rounded'>
+            <input className=' absolute -top-1 -right-1' type='checkbox' value={value} onChange={onChange} checked={check} {...props}/>
+            <div className=' z-20'>
+                {name}
+            </div>  
+        </div>
+        
+    </label>
+  )
+}
