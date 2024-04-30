5968007f6c2cc95d62e34495d4301f7d2d14d50c
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-04-30 17:22:33 -0300

    fix: bugs no card

--- src/components/Form/employee/index.jsx
+++ src/components/Form/employee/index.jsx
@@ -8,28 +8,51 @@ import { InputRadioInformation } from '../../Inputs/input-radio-information';
 
 
 export const EmployeeForm = () => {
-    const { AddEmployee, EmployeeData } = React.useContext(EmployeeContext);
+    const { AddEmployee } = React.useContext(EmployeeContext);
     const {setIsOpenModal} = React.useContext(modalContext)
     const {Notification} = React.useContext(toastifyContext)
     const [email, setEmail ] = React.useState('')
     const [name, setName ] = React.useState('')
     const [nif, setNif ] = React.useState('')
     const [nivel_acesso, setNivel_acesso ] = React.useState('')
+    const [error, setError ] = React.useState(false)
+    const [errorMessage, setErrorMessage] = React.useState('');
 
     const [finishForm, setFinishForm] = React.useState(false)
 
     const handleEmailChange = (event) => {
       setEmail(event.target.value);
+      setError(false)
     };
 
     const handleNameChange = (event) => {
       setName(event.target.value);
+      setError(false)
     };
     
     const handleNifChange = (event) => {
       setNif(event.target.value);
     };
     
+
+   function handleNifBlur(){
+     if(nif.length < 5) {
+       setErrorMessage('O  NIF deve conter, no mínimo, 5 caracteres')
+       setError(true)
+       return;
+     }
+
+    }
+
+    function handleNameChangeBlur(){
+      if (!email || !name) {
+        setErrorMessage('Preencha todos os campos obrigatórios!');
+        setError(true); 
+        return;
+      } 
+
+    }
+
     
     const handleSubmit = async (event) => {
       event.preventDefault()
@@ -39,14 +62,8 @@ export const EmployeeForm = () => {
         finalEmail = email + '@senaisp.edu.br';
       }
 
-      console.log(nif)
-      console.log(finalEmail)
-      console.log(name)
-      console.log(nivel_acesso)
-
       const sucess = await AddEmployee(nif, name, finalEmail, nivel_acesso);
-
-        console.log(sucess);
+      
         if(sucess) {
           setFinishForm(true)
           setTimeout(() =>{
@@ -57,7 +74,7 @@ export const EmployeeForm = () => {
           setTimeout(() =>{
             setIsOpenModal(true)
           },[3000])
-          Notification("sucess", "Funcionário não foi criado")
+          Notification("error", "Funcionário não foi criado")
         }
 
     }
@@ -66,23 +83,25 @@ export const EmployeeForm = () => {
     <>
 
         <form className='flex flex-col gap-4 mt-4' onSubmit={handleSubmit}> 
-            <InputText disabled={finishForm} value={name} onChange={handleNameChange} name="Nome completo:" id="nome" text="nome" />
+            <InputText onBlur={handleNameChangeBlur} error={error} disabled={finishForm} value={name} onChange={handleNameChange} name="Nome completo:" id="nome" text="nome" />
 
             <div className='relative'>
               <span className={`absolute inset-y-0 right-4 top-[33px] flex items-center text-ct3 md:text-fun2 text text-roxo-50 p-1 md:p-2 bg-gradient-to-r from-[#BD3FD1] to-[#9332AE] rounded-lg w-auto h-8`}>@senaisp.edu.br</span>
-              <InputText disabled={finishForm} value={email} errorValidacao={email.error} onChange={handleEmailChange}  name="Email:" id="email" text="email" placeholder="ex:marlene" />
+              <InputText disabled={finishForm} onBlur={handleNameChangeBlur} error={error} value={email} errorValidacao={email.error} onChange={handleEmailChange}  name="Email:" id="email" text="email" placeholder="ex:marlene" />
             </div>
             
-            <InputText disabled={finishForm} value={nif} onChange={handleNifChange} name="NIF:" id="nif" text="nif" placeholder=" NN.NNN.NNN/NNNN-NN" />
+            <InputText onBlur={handleNifBlur} error={error} disabled={finishForm} value={nif} onChange={handleNifChange} name="NIF:" id="nif" text="nif" placeholder=" NN.NNN.NNN/NNNN-NN" />
+            
+
 
             <div className='flex gap-2'>
               <InputRadioInformation
                       id="opcao1"
                       value="opcao1"
-                      checked={nivel_acesso === 1}
-                      onChange={() => setNivel_acesso(1)}
+                      checked={nivel_acesso === 3}
+                      onChange={() => setNivel_acesso(3)}
                       placeholder="Administrador"
-                      description="✔ Adicionar usuários, ✔ Remover usuários, ✔ Adicionar alunos ✔ Editar alunos "
+                      description="bla bla bla"
                       disabled={finishForm}
               />
 
@@ -99,8 +118,8 @@ export const EmployeeForm = () => {
               <InputRadioInformation
                       id="opcao3"
                       value="opcao3"
-                      checked={nivel_acesso === 3}
-                      onChange={() => setNivel_acesso(3)}
+                      checked={nivel_acesso === 1}
+                      onChange={() => setNivel_acesso(1)}
                       placeholder="Associado"
                       description="Descrição da opção 3"
                       disabled={finishForm}
