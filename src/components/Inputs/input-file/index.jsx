5d5ba7d4a65cedd24e24b365f2c271245590d038
ryanNS3 <ryansantosn333@gmail.com>
2024-05-06 17:10:18 -0300

    feat: indice de imagem adicionado

--- src/components/Inputs/input-file/index.jsx
+++ src/components/Inputs/input-file/index.jsx
@@ -3,12 +3,13 @@ import { UploadImageIcon } from '../../../assets/uploadImage';
 import { useDropzone } from 'react-dropzone';
 
 
-export function InputImage({value, setValue,file, id , ...props} ) {
+export function InputImage({value, small=false, setValue,file, id , ...props} ) {
 
   const onDrop = React.useCallback((file) =>{
-    setValue(file)
-    console.log(value)
+    setValue(file[0])
   })
+
+  
 
   const dropzone = useDropzone({
     onDrop,
@@ -19,20 +20,29 @@ export function InputImage({value, setValue,file, id , ...props} ) {
 
     }
   })
-   
-  if (file) return <div></div>
-  return (
-    <>
-   
-      <label htmlFor={id} className=' flex flex-col justify-center items-center h-full bg-cinza-100'  {...dropzone.getRootProps()}>
-        <input  className='opacity-0 hidden' id={id}   type='file' {...dropzone.getInputProps()} {...props}/>
-          <UploadImageIcon/>
-          <p>Adicionar imagem</p>
-      </label>
-    </>
-    )
-    
-}
+ 
+
+  
+    return (
+      <>
+        <label htmlFor={id} className=' flex flex-col justify-center h-[500px] roudend items-center bg-cinza-100'  {...dropzone.getRootProps()}>
+          <input  className='opacity-0 hidden' id={id}   type='file' {...dropzone.getInputProps()} {...props}/>
+            {!small &&
+            <>
+              <UploadImageIcon/>
+              <p>Adicionar imagem</p>
+            
+            </>
+            
+            }
+        </label>
+      </>
+      )
+      
+
+
+  }
+
 
 
 export function InputFile({value, setValue, file, id, ...props}){
