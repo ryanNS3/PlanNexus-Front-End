b7946b9675011e847bc2c20ed2ab48b11da781fa
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-05-08 17:14:59 -0300

    feat: editedEmployee

--- src/components/EmployeeDetails/index.jsx
+++ src/components/EmployeeDetails/index.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState, useContext } from 'react';
 import { InputText } from '../Inputs/input-text/inputTextComp';
 import { EmployeeContext } from "../../context/Employee";
+import { PinkButton } from '../Buttons/pinkButton';
 
 export function EmployeeDetails({ employee }) {
     const { EditEmployee, EmployeeData } = useContext(EmployeeContext);
     const [editedEmployee, setEditedEmployee] = useState(employee);
 
+    console.log(employee)
+    const editedData = [
+        {
+            "idFuncionario": employee.id_funcionario,
+            "NIF": employee.NIF,
+            "nome": employee.nome,
+            "email": employee.email,
+            "nivel_acesso": "3",
+            "foto": "null"
+        }
+    ]
+
       const handleSubmit = async () => {
-        const success = await EditEmployee({
-            Id: employee.NIF,
-            NIF: editedEmployee.NIF,
-            nome: editedEmployee.nome,
-            email: editedEmployee.email,
-            nivel_acesso: editedEmployee.nivel_acesso,
-            foto: editedEmployee.foto
-        });
+        const success = await EditEmployee(editedData);
         console.log(success)
 
         if (success) {
@@ -26,21 +32,31 @@ export function EmployeeDetails({ employee }) {
     console.log(employee)
         return (
             <div>
-                <p>{employee.nome}</p>
-                <p>{employee.nome_cargo}</p>
-                <p>Informações pessoais:</p>
+                <div className='flex items-center gap-4 mb-4'>
+                    <div className='w-12 h-12 rounded-full bg-cinza-300 flex items-center justify-center overflow-hidden '></div>
+                    <div>
+                        <p className='text-h5'>{employee.nome}</p>
+                        <p className='text-cp2'>{employee.nome_cargo}</p>
+                    </div>
+                </div>
+                
+                
+                <p className='text-rosa-500 text-fun2 border-l-2 border-rosa-500 pl-1 mb-4'>Informações pessoais:</p>
                 <div className='flex flex-wrap gap-4'>
                     <InputText id="Nome" name="Nome:" placeholder={employee.nome} disabled/>
                     <InputText id="NIF" name="NIF:" placeholder={employee.NIF} disabled/>
                     <InputText id="Email" name="Email:" placeholder={employee.email} disabled/>
                 </div>
 
-                <p>Informações de cargo:</p>
+                <p className='text-rosa-500 text-fun2 border-l-2 border-rosa-500 pl-1 my-4'>Informações de cargo:</p>
                 <div className='flex flex-wrap gap-4'>
                     <InputText id="Nome" name="Cargo:" placeholder={employee.nome_cargo} disabled/>
                 </div>
 
-                <button onClick={handleSubmit}>Salvar Alterações</button>
+                <div className='flex gap-4 mt-20'>
+                    <PinkButton type='secondary' text='Desabilitar perfil' action={handleSubmit}/>
+                    <PinkButton text='Restaurar senha'/>
+                </div>
             </div>
         );
     
