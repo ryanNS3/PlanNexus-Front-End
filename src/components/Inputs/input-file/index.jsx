4bd138904e3ddcfcdc83c690c0fa92e9acc06704
ryanNS3 <ryansantosn333@gmail.com>
2024-05-13 10:52:33 -0300

    feat: Adicionando a cor como chave na imagem

--- src/components/Inputs/input-file/index.jsx
+++ src/components/Inputs/input-file/index.jsx
@@ -3,31 +3,40 @@ import { UploadImageIcon } from '../../../assets/uploadImage';
 import { useDropzone } from 'react-dropzone';
 
 
-export function InputImage({value, small=false, setValue, id , indice, ...props} ) {
+export function InputImage({value, small=false,disabled=true, setValue,keyForImage,indexForColor, id , indice, ...props} ) {
   const onDrop = React.useCallback((file) =>{
     let files = [...value];
-    files[indice].push(file)
+    files[indice].push({[keyForImage]:{file}})
     setValue(files)
-  },[])
-  console.log(value[0].length)
+  },[keyForImage])
+  console.log(keyForImage)
 
+  
   const dropzone = useDropzone({
     onDrop,
     accept:{
       'image/png' : ['.png'],
       'image/jpg' : ['.jpg'],
       'image/svg' : ['.svg']
-
+      
     }
   })
+  const {isDragActive} = dropzone;
+  
 
-    if (value[indice].length > 0) return <RenderImage file={value[indice][0][0]}/>
+//                                                          0-array 1-posicao 2-indice da cor 3-chave da cor 4-array de arquivos 5-arquivo 
+    if (value[indice].length > 0) return <RenderImage file={value[indice][0][keyForImage].file[0]}/>
  
     
     return (
       <>
-        <label htmlFor={id} className=' flex flex-col justify-center h-[500px] rounded-lg items-center border-dotted border-4 border-cinza-100'  {...dropzone.getRootProps()}>
+        <label htmlFor={id} className={`flex flex-col justify-center h-[500px] rounded-lg items-center border-dotted border-4 border-cinza-100 hover:border-rosa-300`} 
+          {...disabled ? null : {...dropzone.getRootProps()}} >
+          {disabled ?
+          <input id={id} disabled/>
+          :
           <input  className='opacity-0 hidden' id={id} {...dropzone.getInputProps()} {...props}/>
+          }
             <>
             <UploadImageIcon/>
             {!small &&  <p className=' max-w-[20ch] text-center'> <span className=' text-fun2'>Arraste</span> ou clique aqui para adicionar uma imagem</p>}
