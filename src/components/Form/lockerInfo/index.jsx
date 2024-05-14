d036dac901ec9de140564e358809e8ce823432c2
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-05-14 01:50:57 -0300

    feat: add Locker component and style fixed

--- src/components/Form/lockerInfo/index.jsx
+++ src/components/Form/lockerInfo/index.jsx
@@ -1,26 +1,28 @@
-import { Locker } from "../../AllLocker";
-import { AllLocker } from "../../AllLocker";
+
 import { PinkButton } from "../../Buttons/pinkButton";
 import { SecundaryButton } from "../../Buttons/secundaryButton";
 import { Volunteer } from "../../../assets/Volunteer";
 import { Lock } from "../../../assets/Lock";
 
-export function LockerInfo({ numero }) {
+export function LockerInfo({ nome, numero, status }) {
+
+    
 
     return (
         <form action="submit" className="flex flex-col w-full h-full justify-between">
             <div>
-                <div className={'flex h-24 max-w-24 rounded-lg bg-cinza-400 '}>
-                    <p>{numero}</p>
+                <div className={`relative max-w-24 ${status == "ocupado" ? "bg-[#A0E29E]" : "bg-cinza-100"
+                    } h-24 flex items-center justify-center rounded-lg`}>
+                    <p className="text-h5">{numero}</p>
                 </div>
                 <h1 className="text-h4 mt-4">Armário {numero}</h1>
                 <h3 className="text-sub1 mt-8">Disponibilidade:</h3>
-                <p className="mt-4">Disponível</p>
+                <p className="mt-4">{nome == null ? 'Indisponível' : 'Disponível'}</p>
             </div>
 
-            <div className="flex items-end justify-end gap-x-4">
-                <SecundaryButton text={'Trancar Armário'} icon={<Lock />} />
-                <PinkButton text={'Doar Armário'} icon={<Volunteer />} />
+            <div className="flex gap-2 items-end justify-between gap-x-4">
+                <SecundaryButton text={'Trancar Armário'} icon={<Lock />} size={'big'} />
+                <PinkButton text={'Doar Armário'} icon={<Volunteer />} size={'big'}/>
             </div>
         </form>
     )
