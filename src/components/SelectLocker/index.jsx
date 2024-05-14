82606a68728b0df1c3e383195af8e2aee20fa4ab
GeoGustin <geogustinad@gmail.com>
2024-05-14 17:21:02 -0300

    feat: finish SelectLocker

--- src/components/SelectLocker/index.jsx
+++ src/components/SelectLocker/index.jsx
@@ -1,15 +1,20 @@
-import { Lockers } from './../AllLocker'
-import { SquareCheckBox } from './../Inputs/CheckBox'
+import { Lockers } from "./../AllLocker";
+import { CheckBox } from "./../Inputs/input-CheckBox/index";
 
 export function SelectLocker() {
-    return (
-        <>
-            <div className="flex flex-col flex-wrap">
-                <h1>Selecione o armário</h1>
-                <div className='flex flex-col h-3/5 bg-branco px-6 rounded-lg overflow-y-auto'>
-                    <Lockers size={'small'} />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className="flex flex-col flex-wrap mt-36">
+      <h1 className="text-h5">Selecione o armário</h1>
+      <div className="flex flex-wrap gap-x-2 items-center mt-5">
+        <CheckBox />
+        <p className="text-fun2"> Selecionar tudo</p>
+      </div>
+      <div
+        className="flex flex-col h-3/5 bg-branco px-6 rounded-lg overflow-y-auto mt-3"
+        style={{ scrollbarWidth: "none" }}
+      >
+        <Lockers size={"small"} />
+      </div>
+    </div>
+  );
 }
