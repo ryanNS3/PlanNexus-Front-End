0760a0680a9e8af787cdec60334d23b98473375e
ryanNS3 <ryansantosn333@gmail.com>
2024-05-15 10:45:03 -0300

    fix: melhorando os estilos da label

--- src/components/Inputs/Label/index.jsx
+++ src/components/Inputs/Label/index.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export function Label({text, id, children}) {
   return (
-    <label className='flex flex-col text-fun2 w-full capitalize mb-4' htmlFor={id}>{text}</label>
+    <label className='flex flex-col text-fun2 w-full capitalize mb-2' htmlFor={id}>{text}</label>
   )
 }
