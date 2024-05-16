d19bb9c14618789ae92dae45b0c5bb5fb7070e3a
LaizaBarbosa <131712297+LaizaBarbosa@users.noreply.github.com>
2024-05-16 16:37:53 -0300

    style: consertado espaçamento entre os cards

--- src/components/aapmManagement/index.jsx
+++ src/components/aapmManagement/index.jsx
@@ -7,7 +7,7 @@ import { GiftCard } from "./GiftCard"
 export function AapmManage(){
     return(
         <section>
-            <div className="grid grid-cols-3 mb-10">
+            <div className="grid grid-cols-3 mb-10 gap-2 h-screen">
                 <CardMedium children={ <PriceCard/> } />
                 <CardMedium children={ <GiftCard/> } />
             </div>
