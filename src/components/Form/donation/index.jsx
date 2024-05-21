d0d899f3bee012a69f843e8caf7224fe4b871f66
LaizaBarbosa <laiza0700@gmail.com>
2024-05-21 17:19:01 -0300

    feat: criado form de doação

---
+++ src/components/Form/donation/index.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { InputText } from "../../Inputs/input-text/inputTextComp";
+import { InputRadioInformation } from "../../Inputs/input-radio-information";
+
+
+export function MakeDonation(){
+  const steps = [
+    'Informações do aluno',
+    'Produto para doação',
+    'Contrato'
+  ]
+
+  const [name, setName] = React.useState(null)
+  const [email, setEmail] = React.useState(null)
+  const [cpf, setCpf] = React.useState(null)
+  const [role, setRole] = React.useState()
+
+  const [currentStep, setCurrentStep] = React.useState(1)
+  function setStep(step){
+    setCurrentStep(step)
+  }
+
+  return(
+    <div>
+        <h3>DOAÇÃO</h3>
+        <nav>
+          <ul>
+            {steps?.map((step, index) => {
+              <React.Fragment key={index}>
+                <li key={index} onClick={() => name && email && cpf && role ? setStep(index + 1) : null}>
+                  <div className={`flex rounded-full w-11 h-11 text-fun2 items-center justify-center ${
+                      currentStep == index + 1
+                        ? 'bg-rosa-300 text-rosa-50'
+                        : 'bg-[#D9D9D9] text-cinza-700'
+                    }`}>
+                      <span className={`block ${
+                      currentStep == index + 1 ? 'text-preto' : 'text-cinza-500'
+                    } text-fun2 max-w-28 text-center`}>
+                        {step}
+                      </span>
+                  </div>
+                </li>
+
+                {index != steps.length -1 ? (
+                  <div className="w-screen h-[2px] bg-cinza-300 mt-[21px]"></div> 
+                ) : null}
+
+                <form>
+                  <Step currentStep={currentStep} step={1}>
+                    <div>
+                      <InputText id='name' type='text' name='Nome completo' placeholder='José da Silva' onChange={(e) => setName(e.target.value)} value={name ? name : ''} />
+
+                      <InputText id='email' type='email' name='Email' placeholder='jose@gmail.com' onChange={(e) => setEmail(e.target.value)} value={email ? email : '' } />
+
+                      <InputText id='cpf' type='text' name='CPF' placeholder='000.000.000-00' onChange={(e) => setCpf(e.target.value)} value={cpf ? cpf : ''} />
+
+                      <InputText id='cellphone' type='text' name='telefone' placeholder='55 11 111111111' onChange={(e) => setCellphone(e.target.value)} value={cellphone ? cellphone : ''} />
+                    </div>
+                  </Step>
+
+                  <Step currentStep={currentStep} step={2}>
+                    <h4>Produto para doação</h4>
+                  </Step>
+
+                  <Step currentStep={currentStep} step={3}>
+
+                  </Step>
+                </form>
+              </React.Fragment>
+            })}
+          </ul>
+        </nav>
+    </div>
+  )
+}
+
+function Step({currentStep, step, children}) {
+  const show = current === step
+
+  return show ? <>{children}</> : null
+}
