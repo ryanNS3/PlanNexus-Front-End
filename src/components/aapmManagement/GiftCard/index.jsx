02b6a3a50dca721a39eb2357142968b63d330168
LaizaBarbosa <laiza0700@gmail.com>
2024-05-22 17:23:00 -0300

    lógica trocar brinde atualizada

--- src/components/aapmManagement/GiftCard/index.jsx
+++ src/components/aapmManagement/GiftCard/index.jsx
@@ -1,35 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {DonationIcon} from '../../../assets/Gestao/donationIcon'
 import {RepeatIcon} from "../../../assets/Gestao/repeat"
 import { CardMedium } from "../../Cards/Card";
 import { ProductContext } from "../../../context/ProductContext";
 import BasicModal from "../../Modal";
-import { useMutation } from "@tanstack/react-query";
+import { PinkButton } from "../../Buttons/pinkButton";
 
-const resOneProduct = [
-  {
-    "id": 1,
-    "nome": "Produto A",
-    "brinde": false
-  },
-  {
-    "id": 2,
-    "nome": "Produto B",
-    "brinde": true
-  },
-  {
-    "id": 3,
-    "nome": "Produto C",
-    "brinde": false
-  }
-]
 
 export function GiftCard(){
   const [isOpenDrop, setOpenDrop] = React.useState(false);
   const [modal, setModalOpen] = React.useState(false)
   const [gift, setGift] = React.useState(false)
-  const  {GetGiftProduct}  = React.useContext(ProductContext)
-  // const { resOneProduct } = GetGiftProduct()
+  const  {GetGiftProduct, GetProducts, SwitchGift}  = React.useContext(ProductContext)
+  const { resOneProduct } = GetGiftProduct()
 
     const handleOpenDropDown = () => {
         setOpenDrop(!isOpenDrop);
@@ -39,35 +22,68 @@ export function GiftCard(){
         setGift()
     };
 
+    const { resProductData } = GetProducts()
+
+    const handleNewGift = async(productId) => {
+    try {
+      await SwitchGift.mutateAsync(productId)
+    } catch (error) {
+      console.log('erro ao ativar brinde: ', error)
+    }
+  }
+
     return(
         <div>
             <div className="mb-10 flex align-center" >
                 <DonationIcon/> 
                 <h4 className="text-sub1 text-cinza-950 mt-1 ml-2" >Brinde do semestre</h4>
             </div>
-            { resOneProduct && 
+            { resOneProduct && resOneProduct.json.response.map((brindeAtivo, key) => {
 
-              <div className="pt-4" >
-                <div className="flex align-center mt-2 border-2 border-cinza-100 rounded-lg p-2 w-">
-                    <img src={resOneProduct[0].foto} className="w-14"/>
-                    <p className="text-fun2 mt-2 ml-2" >{resOneProduct[0].nome}</p> 
+              return(
+
+                <div className="pt-4" >
+                <div key={key} className="flex align-center mt-2 border-2 border-cinza-100 rounded-lg p-2 w-">
+                    <img src={brindeAtivo.brinde ? brindeAtivo.foto : ''} className="w-14"/>
+                    <p className="text-fun2 mt-2 ml-2" >{brindeAtivo.brinde ? brindeAtivo.nome : ''}</p> 
 
                 </div>
                 <Dropdown
-                trigger={<button className="flex relative -top-[9rem] lg:-right-[15rem] md:-right-[16rem]" onClick={handleOpenDropDown}><RepeatIcon/></button>}
+                trigger={<button className="flex relative -top-[10rem] lg:-right-[15rem] md:-right-[16rem]" onClick={handleOpenDropDown}><RepeatIcon/></button>}
                 open={open}
                 menu={[
                   <button onClick={handleMenuOne} className="w-full flex align-center mb-6 border-2 border-cinza-100 rounded-lg p-2">
-                        <img src={resOneProduct[1].foto} className="w-14" />
-                        <p className="text-fun2 mt-2 ml-2" >{resOneProduct[1].nome}</p> 
+                        <img src={brindeAtivo.foto} className="w-14" />
+                        <p className="text-fun2 mt-2 ml-2" >{brindeAtivo.nome}</p> 
                     </button>,
+
                     <BasicModal labelButton="Escolher novo brinde" isOpenModal={modal} setIsOpenModal={setModalOpen} TextButton="Novo brinde" >
                       {/* lista dos produtos */}
-                      <GiftList/>
+                      <div>
+                      <h4 className="text-h4">Escolher novo brinde</h4>
+                      {resOneProduct && resOneProduct.json.response.map((pd, key) => {
+                          return(
+                            <div>
+                              <div>
+                                
+                                
+                                  <img src={pd.foto} className='w-14'/>
+                                  <p key={key} className="flex align-center gap-4"> {pd.nome} <p className="text-fun2 text-rosa-300">{pd.brinde ? 'brinde atual' : ''}</p> </p>
+                                
+
+                              </div>
+                          </div>
+                        )
+                      })}
+                      <PinkButton action={handleNewGift} text='Confirmar' typeButton='button' />
+                      </div>
+                      
                     </BasicModal>
                 ]}
                 /> 
             </div> 
+              )
+              })
             }
         </div>
     )
@@ -110,22 +126,3 @@ const Dropdown = ({ trigger, menu }) => {
 
 
 
-  const GiftList = () => {
-    const  {GetGiftProduct, SwitchGift}  = React.useContext(ProductContext)
-    // const { resOneProduct } = GetGiftProduct()
-
-    // const handleNewGift = async(productId) => {
-    //   try {
-    //     await SwitchGift.mutateAsync(productId)
-    //   } catch (error) {
-    //     console.log('erro ao ativar brinde: ', error)
-    //   }
-    // }
-    
-    {resOneProduct && resOneProduct.map(() => {
-          return(
-          <p key={resOneProduct.id} > {resOneProduct.nome} - {resOneProduct.brinde ? 'brinde atual' : ''} </p>
-          )
-        })}
-    
-  }
