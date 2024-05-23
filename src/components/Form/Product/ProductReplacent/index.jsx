f1432fdd400c415f66c271e16ef01b87c38bb7d7
Pablo oliveira <pablo_drgoes@hotmail.com>
2024-05-23 17:17:12 -0300

    feat/ adicionando cores e tamanhos na reposição de estoque

--- src/components/Form/Product/ProductReplacent/index.jsx
+++ src/components/Form/Product/ProductReplacent/index.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { SquareCheckBox } from '../../../Inputs/input-CheckBox';
+import { Lock } from '../../../../assets/Lock';
 
-export function ProductReplacent({sizes, color}) {
+export function ProductReplacent({product}) {
+    const [selectColor, setSelectColor] = React.useState(null)
+    const [idProductSize, setProductSize] = React.useState([])
+
+
+    function handleSelectColor(event){
+        console.log(event)
+    }
+
   return (
       <>
-          <h1 className=' text-h4 uppercase'>Repor estoque</h1>
-          
-          <section>
+          <h1 className=' text-h4 mb-8 uppercase'>Repor estoque</h1>
+          <section className=' space-y-4'>
               <h2 className=' text-sub2'>Seleciona uma cor</h2>
+              <div className='flex gap-5'>
+                {product && 
+                    product.map((color) =>{
+                        return(
+                            <article>
+                                <SquareCheckBox onChange={handleSelectColor}>
+                                    <img className=' w-full' src={color.fotos[0]}/>
+                                </SquareCheckBox>
+                                <p className=' text-fun2'>{color.cor}</p>
+                            </article>
+                        )
+                    })
+                }
+
+              </div>
+          </section>
+
+          <section>
+            <div className='flex items-center gap-2'>
+                <h1 className=' text-sub2'>Tamanhos</h1>
+                <Lock black/>
+            </div>
+           
           </section>
       </>
   )
