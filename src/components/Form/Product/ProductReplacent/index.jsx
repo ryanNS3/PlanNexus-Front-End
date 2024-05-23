577e0a1dbc5e6e4924b7f8e923739cbaf80300c7
ryanNS3 <ryansantosn333@gmail.com>
2024-05-23 09:49:42 -0300

    feat/ criação do formulário de reposição de estoque

---
+++ src/components/Form/Product/ProductReplacent/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function ProductReplacent({sizes, color}) {
+  return (
+      <>
+          <h1 className=' text-h4 uppercase'>Repor estoque</h1>
+          
+          <section>
+              <h2 className=' text-sub2'>Seleciona uma cor</h2>
+          </section>
+      </>
+  )
+}
