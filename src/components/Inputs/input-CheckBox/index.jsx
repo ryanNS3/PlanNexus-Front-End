21c20a8bb91459f41b5d4fe6e7146c383513c0eb
Pablo oliveira <pablo_drgoes@hotmail.com>
2024-05-23 17:17:25 -0300

    fix: Ajustando responsividade

--- src/components/Inputs/input-CheckBox/index.jsx
+++ src/components/Inputs/input-CheckBox/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export function SquareCheckBox({name, value , setvalue, check, onChange, ...props}){
+export function SquareCheckBox({name,children, value , setvalue, check, onChange, ...props}){
   return (
     <label className=' block relative '> 
-        <div className='py-4 px-5 bg-cinza-100 rounded'>
+        <div className='rounded p-2 size-18 bg-cinza-100'>
             <input className=' absolute -top-1 -right-1' type='checkbox' value={value} onChange={onChange} checked={check} {...props}/>
             <div className=' z-20'>
-                {name}
+               {children}
             </div>  
         </div>
         
