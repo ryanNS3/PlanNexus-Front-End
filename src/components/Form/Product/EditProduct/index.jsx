70938fecaecf97a8d00f1e6dcaeaaaf9e89c52fa
ryanNS3 <ryansantosn333@gmail.com>
2024-05-23 09:49:11 -0300

    feat/ criação do formulário para a edição de produtos

--- src/components/Form/Product/EditProduct/index.jsx
+++ src/components/Form/Product/EditProduct/index.jsx
@@ -6,11 +6,36 @@ import { TextArea } from "../../../Inputs/TextArea";
 import { Label } from "../../../Inputs/Label";
 import { GhostButton } from "../../../Buttons/ghostButton";
 import { Square } from "../../../square";
+import { PinkButton } from "../../../Buttons/pinkButton";
 
 
-export function EditProductForm({name, preco, allSize, allColor}){
-    const [nameProduct, setNameProduct] = React.useState(name)
-    const [priceProduct, setPriceProduct] = React.useState(preco)
+export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
+    const [nameProduct, setNameProduct] = React.useState(dataProduct.nome) 
+    const [priceProduct, setPriceProduct] = React.useState(dataProduct.produtos[0].tamanhos[0].valor)
+
+    const [originalDataProduct, setOriginalDataProduct] = React.useState([{
+        nome : dataProduct.nome,
+        valor: dataProduct.produtos[0].tamanhos[0].valor,
+        tamanhos : [dataProduct.produtos[idColor].tamanhos?.map((size) => size.tamanho)]
+    }])
+    const [isEditing, setIsEditing] = React.useState({
+        nome: false,
+        valor: false,
+        email: false
+    });
+
+    function handleCloseEdit(event) {
+        event.preventDefault()
+        setIsEditForm(false)
+
+    }
+    const handleEditClick = (field) => {
+        setIsEditing((prev) => ({
+          ...prev,
+          [field]: !prev[field]
+        }));
+      };
+    
     return(
         <form>
             <div className=" flex">
@@ -18,10 +43,10 @@ export function EditProductForm({name, preco, allSize, allColor}){
                 <input type="checkbox" />
             </div>
 
-            <DecorationLine my={"my-4"}/>
+            <DecorationLine my={"my-6"}/>
 
-            <section className=" mb-11" aria-labelledby="informacoesGeraisProduto" >
-                <h2 className=" text-fun2 mb-2 text-rosa-300 after:w-2 after:h-full after:block" id="informacoesGeraisProduto" >INFORMAÇÕES GERAIS:</h2>
+            <section className=" mb-9" aria-labelledby="informacoesGeraisProduto" >
+                <h2 className=" text-fun2 mb-4 text-rosa-300 after:w-2 after:h-full after:block" id="informacoesGeraisProduto" >INFORMAÇÕES GERAIS:</h2>
                 <div className="grid grid-cols-2 gap-x-9 gap-y-4">
                     <EditableInput name="Nome" disabled value={nameProduct}/>
                     <EditableInput name="Preço" disabled value={priceProduct}/>
@@ -32,16 +57,23 @@ export function EditProductForm({name, preco, allSize, allColor}){
                 </div>
             </section>
 
-            <section aria-labelledby="variacoesProduto">
-                <h2 className=" text-fun2 mb-6 text-rosa-300 after:w-2 after:h-full after:block" id="variacoesProduto" >TAMANHOS:</h2>
-                {allSize?.map((size) =>{
-                    return(
-                        <Square>{size}</Square>
-                    )
-                })}
+            <section aria-labelledby="variacoesProduto" className=" mb-9">
+                <h2 className=" text-fun2 mb-5 text-rosa-300 after:w-2 after:h-full after:block" id="variacoesProduto" >TAMANHOS:</h2>
+                <div className=" flex gap-4">
+                    {dataProduct.produtos[idColor].tamanhos?.map((size) => {
+                        return(
+                            <Square>{size.tamanho}</Square>
+                        )
+                    })}
+
+                </div>
             </section>
 
-            <GhostButton/>
+            <div className=" flex gap-5 ">
+                <GhostButton action={handleCloseEdit} text="Cancelar" />
+                <PinkButton text="Confirmar" size="small" />
+            </div>
+            
         </form>
     )
 }
