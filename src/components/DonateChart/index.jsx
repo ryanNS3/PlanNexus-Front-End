1df8a6a83f6c9d741c4d14a7d1f034b4557107fc
joaonishikawa <joaonishikawa@gmail.com>
2024-05-28 01:55:58 -0300

    feat: Donate Page 95%

--- src/components/DonateChart/index.jsx
+++ src/components/DonateChart/index.jsx
@@ -1,10 +1,11 @@
 import { Doughnut, Line } from "react-chartjs-2";
 import { useState, useEffect } from "react";
-import { CardLarge } from "../Cards/Card";
+import { CardMedium } from "../Cards/Card";
 
-const dadosMock = [24, 47, 15];
+const dadosMock = [124, 63, 71];
 const colorsData = ["#D9A5EC", "#45DDA8", "#527661"];
-const labelNames = ["Transporte", "Dinheiro", "Alimentício"];
+
+const dadosMock2 = [58, 26, 128];
 
 const data = {
   labels: [],
@@ -12,6 +13,17 @@ const data = {
     {
       label: "Valor (em R$)",
       data: dadosMock.map((item) => item),
+      backgroundColor: colorsData.map((item) => item),
+    },
+  ],
+};
+
+const data2 = {
+  labels: [],
+  datasets: [
+    {
+      label: "Quantidade",
+      data: dadosMock2.map((item) => item),
       backgroundColor: colorsData.map((item) => item),
     },
   ],
@@ -31,23 +43,61 @@ const config = {
   },
 };
 
-// eixo y do gráfico
-const labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+const config2 = {
+  type: "doughnut",
+  data: data2,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: "right",
+      },
+    },
+  },
+};
 
 // dados do gráfico
 const DATA_COUNT = 3;
-const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
 
 export function DonateChart() {
   return (
     <>
-      <CardLarge>
-        <div className=" max-w-[350px]">
+      <CardMedium>
+        <div className=" max-w-[250px]">
             <h2 className="text-sub2">Custo de Doação</h2>
-            <Doughnut  options={config} data={data} />
-
+            <div className="flex">
+              <Doughnut  options={config} data={data} />
+              <div className="flex-col p-10">
+                <div className="flex items-center gap-1"><div className="w-4 h-4 rounded bg-[#D9A5EC]"></div>Transporte</div>
+                <div className="flex items-center gap-1"><div className="w-4 h-4 rounded bg-[#45DDA8]"></div>Alimentício</div>
+                <div className="flex items-center gap-1"><div className="w-4 h-4 rounded bg-[#527661]"></div>Dinheiro</div>
+              </div>
+            </div>
         </div>
-      </CardLarge>
+      </CardMedium>
     </>
   );
 }
+
+export function DonateChart2() {
+  return (
+    <>
+      <CardMedium>
+        <div className=" max-w-[250px]">
+            <h2 className="text-sub2">Produtos mais doados</h2>
+            <div className="flex">
+              <Doughnut  options={config2} data={data2} />
+              <div className="flex-col p-10">
+                <div className="flex items-center gap-1"><div className="w-4 h-4 rounded bg-[#D9A5EC]"></div>Camiseta</div>
+                <div className="flex items-center gap-1"><div className="w-4 h-4 rounded bg-[#45DDA8]"></div>Copo</div>
+                <div className="flex items-center gap-1"><div className="w-4 h-4 rounded bg-[#527661]"></div>Caneta</div>
+              </div>
+            </div>
+
+        </div>
+      </CardMedium>
+    </>
+  );
+}
+
