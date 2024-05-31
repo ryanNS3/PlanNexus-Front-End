b7ab165317a2d4b561a38835863b65aa39d463df
ryanNS3 <ryansantosn333@gmail.com>
2024-05-31 02:10:52 -0300

    feat/ adicionando validaçaõ pelo blur na edição de produto

--- src/components/Form/Product/EditProduct/index.jsx
+++ src/components/Form/Product/EditProduct/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { productReduce } from "../../../../reducers/product/reduce";
 import { EditableInput } from "../../../Inputs/input-text/inputTextComp";
 import { DecorationLine } from "../../../decorationLine";
 import { TextArea } from "../../../Inputs/TextArea";
@@ -16,33 +16,76 @@ import { toastifyContext } from "../../../../context/toastifyContext";
 
 export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
     const { mutatePatchProduct } = React.useContext(ProductContext)
+    const [productDataState, dispatch] = React.useReducer(
+        productReduce
+        ,{
+        nameProduct: dataProduct.nome,
+        priceProduct: Number(dataProduct.produtos[idColor].tamanhos[0].valor),
+        descriptionProduct: dataProduct.descricao,
+        discountProduct: dataProduct.desconto_associado,
+        sizeProduct: dataProduct.produtos[idColor].tamanhos?.map((size) => size),
+        colorsProduct: dataProduct.produtos.map((color) => color.cor),
+        selectedColor: dataProduct.produtos[idColor].cor,
+        image: [[], [], [], []],
+        isSize: false
+        })
+    const {nameProduct, priceProduct, discountProduct, descriptionProduct, sizeProduct ,colorsProduct, selectedColor, image} = productDataState
+
     const {Notification} = React.useContext(toastifyContext)
     const [allResponseEditingProduct, setAllResponseEditingProduct] = React.useState([])
     const [isDisabledButtonNotEditing, setIsDisableButtonNotEditing] = React.useState(true)
     const allIdsProduct = dataProduct.produtos.map((product) => product.tamanhos.map((size) => size.id_produto))
     const [editedProduct, setEditedProduct] = React.useState({});
-    console.log(dataProduct)
+    // console.log(dataProduct)
     const colorSelected = dataProduct.produtos[idColor].cor;
-    const images = dataProduct.produtos[idColor].fotos.map((foto) =>{ 
-        return {[colorSelected] : foto}
-    })
+    const sizes = [
+        {
+          size: "PP",
+        },
+        {
+          size: "P",
+        },
+        {
+          size: "M",
+        },
+        {
+          size: "G",
+        },
+        {
+          size: "GG",
+        },
+      ];
+
+    const images = dataProduct.produtos[idColor].fotos.reduce((acc, foto) =>{ 
+        const arrayPhotos = []
+        if (foto != acc){
+            arrayPhotos.push(foto);
+        }
+        return arrayPhotos
+    }, dataProduct.produtos[idColor].fotos[0])
+    // const images = dataProduct.produtos[idColor].fotos.map((foto) =>{ 
+    //     return foto
+    // })
+
+    console.log(images)
   
     const [originalDataProduct, setOriginalDataProduct] = React.useState({
         brinde: dataProduct.brinde,
-        nome: dataProduct.nome,
-        descricao: dataProduct.descricao,
-        desconto: 0,
-        quantidadeEstoque: 0,
-        cor: dataProduct.produtos.map((color) => color.cor),
+        nome: nameProduct,
+        descricao: descriptionProduct,
+        desconto: discountProduct,
+        quantidadeEstoque: dataProduct.qtd_estoque,
+        cor: colorsProduct,
         linksFotoAntiga: images,
-        valor: Number(dataProduct.produtos[idColor].tamanhos[0].valor),
-        tamanho : dataProduct.produtos[idColor].tamanhos?.map((size) => size.tamanho)
+        valor: priceProduct,
+        tamanho : sizeProduct
     })
 
     const [isEditing, setIsEditing] = React.useState({
         nome: false,
         brinde: false,
         valor: false,
+        desconto: false,
         tamanhos: false,
         descricao: false,
         img: false
@@ -50,9 +93,10 @@ export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
 
     function handleSubmitEditingProduct(event) {
         event.preventDefault();
+        console.log(originalDataProduct)
         let sucessOrError = []
         allIdsProduct[0].map((idProduct) => {
-            console.log(idProduct)
+            // console.log(idProduct)
             mutatePatchProduct.mutate({
                 idProduto: idProduct,
                 ...originalDataProduct
@@ -73,17 +117,26 @@ export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
                 }
             })
         })
-        console.log(sucessOrError)
+        // console.log(sucessOrError)
         // verifiacando se todas as requisições foram bem sucedidas
-        
+       
        
     }
-
+    // depois do map de requisições essa função é executada e mostra o toastify
     function finalizeSubmit(results) {
         setAllResponseEditingProduct(results);
-        if (results.every((item) => item)) {
-            Notification("success", "Alteração realizada com sucesso");
+        if (results.every((item) => item == true)) {
+            Notification("sucess", "Alteração realizada com sucesso");
             setAllResponseEditingProduct([])
+            setIsEditing({
+                nome: false,
+                brinde: false,
+                valor: false,
+                desconto: false,
+                tamanhos: false,
+                descricao: false,
+                img: false
+            })
         } else {
             Notification("error", "Ocorreu um erro ao alterar os itens");
             setAllResponseEditingProduct([])
@@ -101,7 +154,13 @@ export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
             ...prevState,
             [name] : value
         }
-        ))
+    ))
+    // console.log('teste', originalDataProduct)
+}
+
+
+function handleBlurChangeEditing(event, typeBlur){
+        handleBlurChangeEditing(event, typeBlur)
     }
 
     function handleChangeEditingGift({ target }) {
@@ -114,8 +173,8 @@ export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
     }
 
     React.useEffect(() => {
-        const { nome, valor, tamanhos, descricao, img } = isEditing
-        const isChekedAllEditingOnFalse = !nome && !valor && !tamanhos && !descricao && !img
+        const { nome, valor,desconto, tamanhos, descricao, img } = isEditing
+        const isChekedAllEditingOnFalse = !nome && !valor && !tamanhos && !descricao && !img && !desconto
         if (isChekedAllEditingOnFalse) {
             setIsDisableButtonNotEditing(true)
         }
@@ -147,6 +206,7 @@ export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
                 <div className="grid grid-cols-2 gap-x-9 gap-y-4">
                     <EditableInput
                         onChange={handleChangeEditingProduct}
+                        onBlur={handleBlurChangeEditing}
                         onEditClick={() => handleEditClick("nome")}
                         disabled={!isEditing.nome}
                         isEditable
@@ -159,8 +219,18 @@ export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
                         onChange={handleChangeEditingProduct}
                         onEditClick={() => handleEditClick("preco")}
                         disabled={!isEditing.preco}
-                        value={isEditing.descricao ? editedProduct.preco : originalDataProduct.valor}
+                        value={isEditing.valor ? editedProduct.preco : originalDataProduct.valor}
                         isEditable
+                    />
+                    <EditableInput
+                        name="desconto"
+                        type="number"
+                        onChange={handleChangeEditingProduct}
+                        onEditClick={() => handleEditClick("desconto")}
+                        disabled={!isEditing.desconto}
+                        value={isEditing.desconto ? editedProduct.desconto : originalDataProduct.desconto}
+                        isEditable
+                    
                     />
                     <div className=" col-span-2">
                         <Label text="Descrição" id="descricao" />
@@ -180,6 +250,7 @@ export function EditProductForm({ dataProduct, setIsEditForm, idColor = 0 }) {
             <section aria-labelledby="variacoesProduto" className=" mb-9">
                 <h2 className=" text-fun2 mb-5 text-rosa-300 after:w-2 after:h-full after:block" id="variacoesProduto" >TAMANHOS:</h2>
                 <div className=" flex flex-wrap gap-4">
+                    
                     {dataProduct.produtos[idColor].tamanhos?.map((size) => {
                         return(
                             <SquareCheckBox value={size.tamanho}>
