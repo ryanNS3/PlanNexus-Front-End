1ccd02ce496761bd0c99d0582b4a5877767bb6f4
LaizaBarbosa <laiza0700@gmail.com>
2024-05-30 21:53:47 -0300

    feat: requisições de post das doações

--- src/context/donatorContext.jsx
+++ src/context/donatorContext.jsx
@@ -10,12 +10,11 @@ export function DonatorProvider({ children }) {
   const BASE_URL = import.meta.env.VITE_API_URL;
   const token = localStorage.getItem('token')
   const user = localStorage.getItem('user')
-  const [DonatorData, setDonatorData] = React.useState(null);
 
-  
-  const GetAllDonators = React.useCallback(async () => {
-    try {
-      const res = await requisicao(
+
+  const FetchAllMoney = React.useCallback(async () => {
+
+      const AllMoneyDonation = await requisicao(
         `${BASE_URL}/doacaoDinheiro/todos`,
         null,
         `GET`,
@@ -24,19 +23,99 @@ export function DonatorProvider({ children }) {
           nif: user,
         }
       );
-      if (res && res.res.status === 200) {
-        setDonatorData(res.json.response);
+      return AllMoneyDonation
+
+});
+
+  const GetMoneyDonation = () => {
+    const AllMoneyDonation = useQuery({ queryKey : ['AllMoneyDonation'], queryFn : FetchAllMoney});
+    const resMoneyData = AllMoneyDonation.data
+    return { resMoneyData };
+  };
+
+
+  const postMoneyDonation = async (dataMoneyDonation) =>{
+    try{
+      const reqMoneyDonation = await requisicao(`${BASE_URL}/doacaoDinheiro/cadastro`, dataMoneyDonation, "POST", {
+        authorization : `bearer ${token}`,
+        nif: user,
+        'Content-Type': 'multipart/form-data'
+      })
+      
+      return reqMoneyDonation
 
     }
-} catch (error) {
-    console.log("Requisição falhou:", error);
-    console.log('falhou')
+    catch (error) {
+      console.log(error)
+      throw new Error(error.message || 'Erro ao fazer a requisição');
+      
     }
-  }, []);
+  }
+
+  const mutateMoneyDonation = useMutation({
+    mutationFn: postMoneyDonation,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['AllMoneyDonation']);
+    }
+  });
+
+
+  const postLockerDonation = async (dataLockerDonation) =>{
+    try{
+      const reqLockerDonation = await requisicao(`${BASE_URL}/doacaoArmario/cadastro`, dataLockerDonation, "POST", {
+        authorization : `bearer ${token}`,
+        nif: user,
+        'Content-Type': 'multipart/form-data'
+      })
+      
+      return reqLockerDonation
+
+    }
+    catch (error) {
+      console.log(error)
+      throw new Error(error.message || 'Erro ao fazer a requisição');
+      
+    }
+  }
+
+  const mutateLockerDonation = useMutation({
+    mutationFn: postLockerDonation,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['AllLockerDonation']);
+    }
+  });
+
+
+  const postProductDonation = async (dataProductDonation) =>{
+    try{
+      const reqProductDonation = await requisicao(`${BASE_URL}/doacaoProduto/cadastro`, dataProductDonation, "POST", {
+        authorization : `bearer ${token}`,
+        nif: user,
+        'Content-Type': 'multipart/form-data'
+      })
+      
+      return reqProductDonation
+
+    }
+    catch (error) {
+      console.log(error)
+      throw new Error(error.message || 'Erro ao fazer a requisição');
+      
+    }
+  }
+
+  const mutateProductDonation = useMutation({
+    mutationFn: postProductDonation,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['AllProductDonation']);
+    }
+  });
+
+
 
   return (
     <DonatorContext.Provider
-      value={{ GetAllDonators }}
+      value={{ GetMoneyDonation, mutateLockerDonation, mutateMoneyDonation, mutateProductDonation }}
     >
       {children}
     </DonatorContext.Provider>
