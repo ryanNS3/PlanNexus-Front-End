3017c739a7e6bc7d7cbb914404ebbbb45355b646
ryanNS3 <ryansantosn333@gmail.com>
2024-05-31 19:16:24 -0300

    feat/ criando actions de produto

--- src/reducers/product/actions.jsx
+++ src/reducers/product/actions.jsx
@@ -1,42 +1,87 @@
-
-
-export function handleBlurEditing(event, nameDataEditing, setError){
+export function handleBlurEditingAction(event, nameDataEditing, setError, dispatch) {
     const { name, value } = event.target;
-    value, setError, name 
-    switch (nameDataEditing){
-        case "nameProduct":
-            dispatch({
-                type: "HANDLE_BLUR_NAME",
-                payload:{
-                    name: name,
-                    value: value,
-                    setError: setError
-                }
-               })
-        
-        case "colorsProduct":
-            dispatch({
-                type: "HANDLE_BLUR_COLORS"
-            })
-        case "priceProduct":
-            dispatch({
-                type: "HANDLE_BLUR_PRICE",
-                payload:{
-                    name: name,
-                    value: value,
-                    setError: setError
 
+    switch (nameDataEditing) {
+        case "nameProduct":
+            return dispatch({
+                type: "HANDLE_BLUR_NAME",
+                payload: {
+                    name: name,
+                    value: value,
+                    setError: setError
                 }
-            })
+            });
+
+        case "colorsProduct":
+            return dispatch({
+                type: "HANDLE_BLUR_COLORS"
+            });
+
+        case "priceProduct":
+            return dispatch({
+                type: "HANDLE_BLUR_PRICE",
+                payload: {
+                    name: name,
+                    value: value,
+                    setError: setError
+                }
+            });
 
         case "discountProduct":
-            dispatch({
-                type: "HANDLE_BLUR_DISCOUUNT",
-                payload:{
+            return dispatch({
+                type: "HANDLE_BLUR_DISCOUNT",
+                payload: {
                     name: name,
                     value: value,
                     setError: setError
                 }
-            })
+            });
+        
+
+        default:
+            return;
+    }
+}
+
+export function handleChangeEditingAction(event, nameDataEditing, payload, dispatch) {
+    switch (nameDataEditing) {
+        case "nameProduct":
+            return dispatch({
+                type: "HANDLE_CHANGE_NAME",
+                payload: payload
+            });
+
+        case "colorsProduct":
+            return dispatch({
+                type: "HANDLE_CHANGE_COLOR",
+                payload: payload
+            });
+
+        case "priceProduct":
+            return dispatch({
+                type: "HANDLE_CHANGE_PRICE",
+                payload: payload
+            });
+
+        case "discountProduct":
+            return dispatch({
+                type: "HANDLE_CHANGE_DISCOUNT",
+                payload: payload
+            });
+
+        case "descriptionProduct":
+            return dispatch({
+                type: "HANDLE_CHANGE_DESCRIPTION",
+                payload: payload
+            });
+
+        case "sizeProduct":
+            return dispatch({
+                type: "HANDLE_CHANGE_SIZE",
+                payload: payload
+            });
+
+        default:
+            return;
     }
 }
