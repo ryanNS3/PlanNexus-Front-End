bc318fd15cf4f6d266b03b3343d6fe17ddcf9385
ryanNS3 <ryansantosn333@gmail.com>
2024-06-01 03:05:11 -0300

    refactor/ adicionando o spread do payload

--- src/reducers/product/actions.jsx
+++ src/reducers/product/actions.jsx
@@ -1,15 +1,10 @@
-export function handleBlurEditingAction(event, nameDataEditing, setError, dispatch) {
-    const { name, value } = event.target;
+export function handleBlurEditingAction(nameDataEditing, payload, dispatch) {
 
     switch (nameDataEditing) {
         case "nameProduct":
             return dispatch({
                 type: "HANDLE_BLUR_NAME",
-                payload: {
-                    name: name,
-                    value: value,
-                    setError: setError
-                }
+                ...payload
             });
 
         case "colorsProduct":
@@ -20,21 +15,13 @@ export function handleBlurEditingAction(event, nameDataEditing, setError, dispat
         case "priceProduct":
             return dispatch({
                 type: "HANDLE_BLUR_PRICE",
-                payload: {
-                    name: name,
-                    value: value,
-                    setError: setError
-                }
+                ...payload
             });
 
         case "discountProduct":
             return dispatch({
-                type: "HANDLE_BLUR_DISCOUNT",
-                payload: {
-                    name: name,
-                    value: value,
-                    setError: setError
-                }
+                type: "HANDLE_BLUR_DISCOUUNT",
+                ...payload
             });
         
 
@@ -48,25 +35,25 @@ export function handleChangeEditingAction(event, nameDataEditing, payload, dispa
         case "nameProduct":
             return dispatch({
                 type: "HANDLE_CHANGE_NAME",
-                payload: payload
+                ...payload
             });
 
         case "colorsProduct":
             return dispatch({
                 type: "HANDLE_CHANGE_COLOR",
-                payload: payload
+                ...payload
             });
 
         case "priceProduct":
             return dispatch({
                 type: "HANDLE_CHANGE_PRICE",
-                payload: payload
+                ...payload
             });
 
         case "discountProduct":
             return dispatch({
                 type: "HANDLE_CHANGE_DISCOUNT",
-                payload: payload
+                ...payload
             });
 
         case "descriptionProduct":
