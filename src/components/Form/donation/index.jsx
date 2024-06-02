69ae869588e901b8ee2ce5b03255d0f2aba8e301
LaizaBarbosa <laiza0700@gmail.com>
2024-06-02 18:10:57 -0300

    select dos produtos para doação

--- src/components/Form/donation/index.jsx
+++ src/components/Form/donation/index.jsx
@@ -1,21 +1,34 @@
-import React, { useContext, useEffect, useCallback } from "react";
+import React, { useContext, useEffect, useCallback, useState } from "react";
 import { InputText } from "../../Inputs/input-text/inputTextComp";
 import { PinkButton } from "../../Buttons/pinkButton";
 import { ProductContext } from "../../../context/ProductContext";
 import { GhostButton } from "../../Buttons/ghostButton";
 import { studentContext } from "../../../context/studentsContext";
 import { useDebounce } from "../../../hooks/useDebounce";
+import { DonatorContext } from "../../../context/donatorContext";
 
 export function DonationForm() {
+  const { SearchStudents } = useContext(studentContext);
   const { GetProducts } = useContext(ProductContext);
   const { resProductData } = GetProducts();
-  const { SearchStudents } = useContext(studentContext);
+  const {mutateProductDonation} = useContext(DonatorContext)
+
+// console.log(resProductData)
+
 
   const steps = [
     "Informações do aluno",
     "Produto para doação",
     "Contrato"
   ];
+
+  const [selectedOption, setSelectedOption] = React.useState('productDonation');
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
 
   const [name, setName] = React.useState('');
   const [cpf, setCpf] = React.useState('');
@@ -25,12 +38,14 @@ export function DonationForm() {
   const [contract, setContract] = React.useState('');
   const [studentId, setStudentId] = React.useState('');
   const [productId, setProductId] = React.useState('');
+  const [productSize, setSize] = React.useState('P')
   const [lockerNumber, setLockerNumber] = React.useState('');
   const [moneyAmount, setMoney] = React.useState('');
   const [search, setSearch] = React.useState('');
+  const [date, setDate] = React.useState(Date.now());
   const [currentStep, setCurrentStep] = React.useState(1);
 
-  const [DonateDebounce] = useDebounce(cpf, 100);
+  const [DonateDebounce] = useDebounce(cpf, 2000);
 
   const setStep = (step) => {
     setCurrentStep(step);
@@ -51,14 +66,61 @@ export function DonationForm() {
     }
   }, [DonateDebounce, searchStudents]);
 
+  const handleSelect = (event, item) => {
+    event.preventDefault();
+    console.log('Item selecionado:', item);
+    setProductId(item);
+  };
+
+
+  const commonDataDonation = { studentId, date, contract };
+  // let postDataDonation = { ...commonDataDonation };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Add your form submission logic here
+    let postDataDonation = { idAluno: studentId, data: date, contrato: contract, produtoId: productId, quantidade: quantity };
+    // setError(false)
+    // setLoading(true)
+
+    try {
+      mutateProductDonation.mutate(postDataDonation)
+    } catch (error) {
+      console.log(error)
+    }
+
+    // if (selectedOption === 'productDonation') {
+    // } else if (selectedOption === 'moneyDonation') {
+    //   postDataDonation = { ...postDataDonation, moneyAmount };
+    // } else {
+    //   postDataDonation = {...postDataDonation, lockerNumber }
+    // }
+
+    // try {
+    //   let donationType;
+    //   switch (selectedOption) {
+    //     case 'productDonation':
+    //       donationType = await postDoacaoProduto(postDataDonation);
+    //       break;
+    //     case 'moneyDonation':
+    //       donationType = await postDoacaoDinheiro(postDataDonation);
+    //       break;
+    //     case 'LockerDonation':
+    //       donationType = await postDoacaoArmario(postDataDonation);
+    //       break;
+    //     default:
+    //       throw new Error('Opção desconhecida');
+    //   }
+    //   setResponseData(donationType);
+    // } catch (error) {
+    //   setError(error.message);
+    // } finally {
+    //   setLoading(false);
+    // }
+
   };
 
   return (
     <div>
-      <h4 className="text-h4 mb-11">DOAÇÃO</h4>
+      <h4 className="text-h4 mb-11">DOAÇÃO</h4> 
       <nav className="mb-9">
         <ul className="flex justify-between">
           {steps?.map((step, index) => (
@@ -91,12 +153,14 @@ export function DonationForm() {
         </ul>
       </nav>
 
-      <form onSubmit={handleSubmit}>
+      <form >
         <Step currentStep={currentStep} step={1}>
           <div className="flex flex-col gap-6">
             <InputText id="cpf" type="text" name="CPF" placeholder="000.000.000-00" onChange={(e) => setCpf(e.target.value)} value={cpf} />
-            {search && search.json.response.map((student, key) => (
-              <p key={key}>{student.cpf}</p>
+            {search && search.json.response.map((student) => (
+              <ul>
+                <button onClick={() => setStudentId(student.id_aluno)}><li> {student.cpf === search ? student.nome : null} </li></button>
+              </ul>
             ))}
             <InputText id="name" type="text" name="Nome completo" placeholder="José da Silva" onChange={(e) => setName(e.target.value)} value={name} />
             <InputText id="email" type="email" name="Email" placeholder="jose@gmail.com" onChange={(e) => setEmail(e.target.value)} value={email} />
@@ -109,16 +173,50 @@ export function DonationForm() {
 
         <Step currentStep={currentStep} step={2}>
           <div>
-            <p className="text-fun2 my-2">Escolha um produto</p>
-            {resProductData && resProductData.json.response.map((product, key) => (
-              <div key={key} className="border border-cinza-100 max-w-20 rounded-lg my-2">
-                <button name="product" onClick={() => setProductId(product.id)}>
-                  <img src={product.foto} alt={product.nome} className="max-w-14" />
-                  {product.nome} - {product.tamanho}
-                </button>
-              </div>
-            ))}
-            <InputText type="number" id="quantity" name="quantidade" onChange={(e) => setQuantity(e.target.value)} value={quantity} />
+            {/* <div>
+            <label>
+            <input
+              type="radio"
+              value="productDonation"
+              checked={selectedOption === 'productDonation'}
+              onChange={handleOptionChange}
+            />
+            Doação de Produto
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="moneyDonation"
+              checked={selectedOption === 'moneyDonation'}
+              onChange={handleOptionChange}
+            />
+            Doação de Dinheiro
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="lockerDonation"
+              checked={selectedOption === 'lockerDonation'}
+              onChange={handleOptionChange}
+            />
+            Doação de armário
+          </label>
+            </div> */}
+
+            
+            {resProductData && resProductData.json.response.map((product)=> {
+              return(
+                <div>
+                  <button key={product.id_produto} onClick={(e) => handleSelect(e, product.id_produto)} >
+                {product.nome}
+              </button>
+
+                </div>
+              )
+            })}
+            <InputText id="quantidade" type="number" name="Quantidade" placeholder="0" onChange={(e) => setQuantity(e.target.value)} value={quantity} />
+            
+
             <div className="mt-4 flex justify-end gap-2">
               <GhostButton action={() => setCurrentStep(currentStep - 1)} text="voltar" />
               <PinkButton text="Continuar" action={() => setCurrentStep(currentStep + 1)} typeButton="button" />
@@ -142,3 +240,4 @@ export function DonationForm() {
 function Step({ currentStep, step, children }) {
   return currentStep === step ? <>{children}</> : null;
 }
+
