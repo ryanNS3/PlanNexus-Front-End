9cf38e0b4bac4aa700e31a6f264232c2b2bfd5fe
samuelleitee <programmer.and.investor@gmail.com>
2024-06-03 15:32:33 -0300

    feat: add toastify notification to provide feedback about student registration through xlsx file

--- src/components/Form/AddMultipleStudents/index.jsx
+++ src/components/Form/AddMultipleStudents/index.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Check, Close } from "../../../assets/Check";
 import { PinkButton } from "../../Buttons/pinkButton";
 import useAxios from "../../../hooks/useAxios";
+import { toastifyContext } from "../../../context/toastifyContext";
+import { modalContext } from "../../../context/modalContext";
 
 export function AddMultipleStudents() {
   const [alunosFile, setAlunosFile] = React.useState();
   const [response, setResponse] = React.useState();
 
   const { requisicao, loading } = useAxios();
+  const { Notification } = React.useContext(toastifyContext);
+  const { setIsOpenModal } = React.useContext(modalContext);
   const BASE_URL = import.meta.env.VITE_API_URL;
   const token = localStorage.getItem("token");
   const user = localStorage.getItem("user");
@@ -15,21 +19,58 @@ export function AddMultipleStudents() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const req = await requisicao(
+    const success = await requisicao(
       `${BASE_URL}/aluno/cadastro/multiplos`,
-      {alunosFile:alunosFile },
+      { alunosFile: alunosFile },
       "POST",
       {
         authorization: `bearer ${token}`,
         nif: user,
-        'Content-Type': 'multipart/form-data'
+        "Content-Type": "multipart/form-data",
       }
     );
 
-    console.log(req);
+    if (success) {
+      setTimeout(() => {
+        setIsOpenModal(false);
+      }, [3000]);
+      setResponse(success.json.response);
+      Notification("sucess", "Alunos cadastrados com sucesso");
+    } else {
+      setTimeout(() => {
+        setIsOpenModal(true);
+      }, [3000]);
+      Notification("error", "Alunos não cadastrados");
+    }
+
+    // console.log("REQ", req.json.response.alunosCadastrados)
   };
 
-  console.log(alunosFile)
+  if (response) {
+    return (
+      <>
+        <p className="text-sub1">Pré Visualização:</p>
+        <div>
+          <header className="flex justify-between px-16 py-4 border-b-2 border-b-cinza-200">
+            <p className="text-fun2">Nome</p>
+            <p className="text-fun2">Status</p>
+          </header>
+          <div className="py-5 flex flex-col gap-5">
+            {response.alunosCadastrados.map((aluno) => (
+              <>
+                <Line nome={aluno.aluno.nome} status={aluno.status} />
+              </>
+            ))}
+            {response.alunosNaoCadastrados.map((aluno) => (
+              <>
+                <Line nome={aluno.aluno.nome} status={aluno.status} />
+              </>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <div className="mt-10">
@@ -128,29 +169,11 @@ export function AddMultipleStudents() {
           />
         </div>
       )}
-
-      {response && (
-        <>
-          <p className="text-sub1">Pré Visualização:</p>
-          <div>
-            <header className="flex justify-between px-16 py-4 border-b-2 border-b-cinza-200">
-              <p className="text-fun2">Nome</p>
-              <p className="text-fun2">Status</p>
-            </header>
-            <div className="py-5 flex flex-col gap-5">
-              <Line />
-              <Line />
-              <Line />
-              <Line />
-            </div>
-          </div>
-        </>
-      )}
     </div>
   );
 }
 
-function Line({}) {
+function Line({ nome, status, foto }) {
   return (
     <div className="p-4 flex items-center justify-between shadow-[0_4px_8px_0_rgba(227,227,227,1)] rounded">
       <div className="flex items-center gap-5">
@@ -159,11 +182,15 @@ function Line({}) {
           alt=""
           className="h-6 w-6"
         />
-        <p className="text-ct3">Nome do aluno</p>
+        <p className="text-ct3">{nome}</p>
       </div>
-      <div className="flex items-center justify-between gap-3 px-3 py-[0.25rem] bg-[#700707] bg-[#346C32] rounded text-branco text-ct3 font-bold">
-        {<Check /> || <Close />}
-        Adicionado
+      <div
+        className={`flex items-center justify-center gap-3 px-3 py-[0.25rem] ${
+          status === "cadastrado" ? "bg-[#346C32]" : "bg-[#700707]"
+        } rounded text-branco text-ct3 font-bold capitalize w-44`}
+      >
+        {status === "cadastrado" ? <Check /> : <Close />}
+        {status === "cadastrado" ? "Cadastrado" : "Não Cadastrado"}
       </div>
     </div>
   );
