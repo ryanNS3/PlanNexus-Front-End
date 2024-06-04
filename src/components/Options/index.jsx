236f3ced88e4a83333e5ad72d792eb95686fb800
PauloBezz <paulo.bezerra2057@gmail.com>
2024-06-04 17:26:42 -0300

    refactor: logic Options

--- src/components/Options/index.jsx
+++ src/components/Options/index.jsx
@@ -4,46 +4,73 @@ import { Door } from "../../assets/Door";
 import { Volunteer } from "../../assets/Volunteer";
 import { LockerInfo } from "../../components/Form/lockerInfo";
 import { ModalOptions } from "../Modal";
-import React from "react";
+import React, { useRef } from "react";
 import { LockerVolunteer } from "../../components/Form/lockerVolunteer";
 import { LockerContext } from "../../context/lockerContext";
+import { useEffect } from "react";
 
 export function Options({ nome, numero, status, idStudent }) {
+  const [alight, setAlight] = React.useState("");
 
   const { UpdateLocker } = React.useContext(LockerContext);
 
   const handleLock = async () => {
-    const isBusyCloset = status == "desocupado" ? "trancado" : "desocupado"
+    const isBusyCloset = status == "desocupado" ? "trancado" : "desocupado";
     try {
       // Atualizar o status do armário para "Trancado" ou "Desocupado"
       const newStatusLocker = {
         numero: numero,
         id_aluno: idStudent,
         nome_aluno: nome,
-        status: isBusyCloset
-      }
+        status: isBusyCloset,
+      };
 
       const req = await UpdateLocker(newStatusLocker);
-      console.log(req)
-
+      console.log(req);
     } catch (error) {
       console.error("Erro ao trancar o armário:", error);
     }
   };
 
+  let menuRefLocker = useRef();
+
+  const screenWidth = window.innerWidth; // Obtém a largura da tela
+
+  // Calcula 10% da largura da tela
+  let screen = screenWidth * 0.8;
+
+  useEffect(() => {
+    const screenWidth = window.innerWidth; // Obtém a largura da tela
+    let screen = screenWidth * 0.8; // Calcula 10% da largura da tela
+    console.log(screen);
+    let right = menuRefLocker.current.getBoundingClientRect().right;
+
+    if (right >= screen) {
+      console.log("offset right", right, numero);
+      setAlight("items-end");
+    }
+  }, [menuRefLocker]);
+
   return (
     <>
-      <div className="absolute flex flex-col z-30 gap-y-1 ">
+      <div
+        className={`absolute flex flex-col ${alight} z-30 gap-y-1`}
+        ref={menuRefLocker}
+      >
         <SecundaryButton text={"Trancar"} icon={<Lock />} action={handleLock} />
 
         <div>
-          <ModalOptions Button={<SecundaryButton text={"Doar"} icon={<Volunteer />} />}>
+          <ModalOptions
+            Button={<SecundaryButton text={"Doar"} icon={<Volunteer />} />}
+          >
             <LockerVolunteer />
           </ModalOptions>
         </div>
 
         <div>
-          <ModalOptions Button={<SecundaryButton text={"Informações"} icon={<Door />} />}>
+          <ModalOptions
+            Button={<SecundaryButton text={"Informações"} icon={<Door />} />}
+          >
             <LockerInfo nome={nome} numero={numero} status={status} />
           </ModalOptions>
         </div>
