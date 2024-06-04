630b2d0e639f291acc480aae358a1b6ffa923451
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2024-06-04 15:04:31 -0300

    fix: tirando console.log do protectRouter

--- src/components/protectRouter/protectRouter.jsx
+++ src/components/protectRouter/protectRouter.jsx
@@ -4,7 +4,6 @@ import { Navigate } from 'react-router-dom'
 
 export const ProtectRouter = ({ children }) => {
   const { userLogin ,token } = useContext(UserGlobal); 
-  console.log(userLogin, token)
   
   return userLogin && token ? children : <Navigate to="/login" />; 
 }
