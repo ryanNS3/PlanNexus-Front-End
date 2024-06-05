92a5f4d2a8c6601043c3227fb633c47bd58f4748
samuelleitee <programmer.and.investor@gmail.com>
2024-06-05 17:22:07 -0300

    feat: create context to make API requests using react-query

--- src/context/studentsContext.jsx
+++ src/context/studentsContext.jsx
@@ -1,34 +1,147 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import useAxios from "../hooks/useAxios";
 import { UserGlobal } from "./userContext";
-import { useQuery,useQueryClient, useMutation } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { toastifyContext } from "./toastifyContext";
 
-export const studentContext = React.createContext()
+export const StudentContext = React.createContext();
 
 export function StudentProvider({ children }) {
-    const BASE_URL = import.meta.env.VITE_API_URL;
-    const queryClient = useQueryClient()
-    const {token, user} = useContext(UserGlobal)
-    const {requisicao} = useAxios()
+  const queryClient = useQueryClient();
 
-    async function FetchAllStudents() {
-        const requestAllStudents = await requisicao(`${BASE_URL}/aluno/todos`, null, "GET", {
-            authorization: `bearer ${token}`,
-            nif: user
-        })
+  const BASE_URL = import.meta.env.VITE_API_URL;
+  const { token, user } = useContext(UserGlobal);
+  const { Notification } = useContext(toastifyContext);
+  const { requisicao } = useAxios();
 
-        return requestAllStudents
+  // GET (/aluno/todos): Recuperar todos os alunos existentes
+
+  const getStudents = async () => {
+    const requestAllStudents = await requisicao(
+      `${BASE_URL}/aluno/todos`,
+      null,
+      "GET",
+      {
+        authorization: `bearer ${token}`,
+        nif: user,
+      }
+    );
+
+    return requestAllStudents;
+  };
+
+  const queryGetStudents = useQuery({
+    queryKey: ["AllStudentsData"],
+    queryFn: getStudents,
+  });
+
+  // POST (/aluno/cadastro/unico): Adiciona um único aluno
+
+  const createStudent = async (data) => {
+    try {
+      const requestCreateStudent = await requisicao(
+        `${BASE_URL}/aluno/cadastro/unico`,
+        data,
+        "POST",
+        {
+          authorization: `bearer ${token}`,
+          nif: user,
+        }
+      );
+
+      if (!requestUpdateStudent.res.status >= 400) {
+        throw new Error(responseData.message || "Erro ao fazer a requisição");
+      }
+
+      return requestCreateStudent;
+    } catch {
+      throw new Error(error.message || "Erro ao fazer a requisição");
     }
-    const AllStudentsData = useQuery({ queryKey : ['AllStudentsData'], queryFn : FetchAllStudents});
-    const resStudentsData = AllStudentsData.data
-    const resStudentsLoading = AllStudentsData.isLoading
-    const resStudentError = AllStudentsData.isError
+  };
 
+  const mutatePostStudent = useMutation({
+    mutationFn: createStudent,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["AllStudentsData"]);
+      Notification("sucess", "Aluno adicionado com sucesso");
+    },
+    onError: () => {
+      Notification("error", "Aluno não adicionado");
+    },
+  });
 
-    return (
-        <studentContext.Provider value={{AllStudentsData}}>
-            {children}
-        </studentContext.Provider>
-    )
-        
+  // POST (/aluno/cadastro/multiplos): Adiciona vários alunos
+
+  const createStudents = async (data) => {
+    try {
+      const requestCreateStudents = await requisicao(
+        `${BASE_URL}/aluno/cadastro/multiplos`,
+        { alunosFile: data },
+        "POST",
+        {
+          authorization: `bearer ${token}`,
+          nif: user,
+          "Content-Type": "multipart/form-data",
+        }
+      );
+
+      if (!requestUpdateStudent.res.status >= 400) {
+        throw new Error(responseData.message || "Erro ao fazer a requisição");
+      }
+
+      return requestCreateStudents;
+    } catch {
+      throw new Error(error.message || "Erro ao fazer a requisição");
+    }
+  };
+
+  const mutatePostStudents = useMutation({
+    mutationFn: createStudents,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["AllStudentsData"]);
+      Notification("sucess", "Aluno atualizado com sucesso");
+    },
+    onError: () => {
+      Notification("error", "Aluno não atualizado");
+    },
+  });
+
+  // PATCH (/aluno/atualizar): Atualizar alunos existentes
+
+  const updateStudent = async (data) => {
+    try {
+      const requestUpdateStudent = await requisicao(
+        `${BASE_URL}/aluno/atualizar`,
+        data,
+        "PATCH",
+        {
+          authorization: `bearer ${token}`,
+          nif: user,
+        }
+      );
+
+      if (!requestUpdateStudent.res.status >= 400) {
+        throw new Error(responseData.message || "Erro ao fazer a requisição");
+      }
+    } catch {
+      throw new Error(error.message || "Erro ao fazer a requisição");
+    }
+  };
+
+  const mutatePatchStudents = useMutation({
+    mutationFn: updateStudent,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["AllStudentsData"]);
+      Notification("sucess", "Aluno atualizado com sucesso");
+    },
+    onError: () => {
+      Notification("error", "Aluno não atualizado");
+    },
+  });
+
+  return (
+    <StudentContext.Provider value={{ mutatePostStudent, mutatePostStudents, mutatePatchStudents }}>
+      {children}
+    </StudentContext.Provider>
+  );
 }
