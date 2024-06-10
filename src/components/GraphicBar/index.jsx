d2fdc4d77f2a30e2c11076ea3be08c2ddd7caa84
LaizaBarbosa <laiza0700@gmail.com>
2024-06-09 22:10:00 -0300

    consumindo rota de gráfico estoque

--- src/components/GraphicBar/index.jsx
+++ src/components/GraphicBar/index.jsx
@@ -1,40 +1,23 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { Bar } from "react-chartjs-2";
 import Chart from "chart.js/auto";
+import { ProductContext } from "../../context/ProductContext";
 
 export function Grafico() {
-  const meses = [
-    { year: "Camiseta Branca" },
-    { year: "Camiseta Preta" },
-    { year: "Avental" },
-    { year: "Copo" },
-    { year: "Mochila" },
-    { year: "Camisa Polo" },
-  ];
+const {getChartData} = useContext(ProductContext)
+const {chartData} = getChartData()
 
-  const vendas = [
-    { numero: 500 },
-    { numero: 200 },
-    { numero: 50 },
-    { numero: 5 },
-    { numero: 75 },
-    { numero: 30 },
-  ];
-
-  console.log(meses.map((itemMes) => itemMes));
-
-  const labels = meses.map((itemMes) => itemMes.year);
+const labels = chartData?.json?.Estoque?.Label || [];
+const dataValues = chartData?.json?.Estoque?.Data || [];
 
   const data = {
     labels: labels,
     datasets: [
       {
-        // nome da tabela
         label: "Vendas por mês",
         backgroundColor: "#DE7FF9",
         borderColor: "#EBA5FF",
-        // Passando os valores númericos via api para a comparação
-        data: vendas.map((itemVenda) => itemVenda.numero),
+        data: dataValues
       },
     ],
   };
